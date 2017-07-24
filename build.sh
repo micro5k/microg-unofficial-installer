e47297621cc0b6afa0748347c753d2ddfc319eaa
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-24 23:10:21 +0200

    Minor changes

--- build.sh
+++ build.sh
@@ -25,6 +25,7 @@ ui_error()
   exit 1
 }
 
+# Detect script dir
 BASEDIR=$(dirname "$0")
 if [[ "${BASEDIR:0:1}" != '/' ]]; then
   if [[ "$BASEDIR" == '.' ]]; then BASEDIR=''; else BASEDIR="/$BASEDIR"; fi
@@ -32,21 +33,22 @@ if [[ "${BASEDIR:0:1}" != '/' ]]; then
   if [[ "$CURDIR" != '/' ]]; then BASEDIR="$CURDIR$BASEDIR"; fi
 fi
 
+# Set filename and version
 FILENAME='microG-unofficial-installer-ale5000'
 VER=$(cat "$BASEDIR/sources/inc/VERSION")
 
 # Create the output dir
 OUT_DIR="$BASEDIR/output"
-mkdir -p "$OUT_DIR" || ui_error "Failed to create the output dir"
+mkdir -p "$OUT_DIR" || ui_error 'Failed to create the output dir'
 
 # Remove previous file
-rm -f "$OUT_DIR/$FILENAME-v$VER.zip" || ui_error "Failed to remove the previous zip file"
+rm -f "$OUT_DIR/$FILENAME-v$VER.zip" || ui_error 'Failed to remove the previous zip file'
 
 # Copy license
-cp -rpf "$BASEDIR/"LICENSE* "$BASEDIR/sources" || ui_error "Failed to copy license"
+cp -rpf "$BASEDIR/"LICENSE* "$BASEDIR/sources" || ui_error 'Failed to copy license'
 
-cd "$BASEDIR/sources" || ui_error "Failed to change folder"
-zip -r9X "$OUT_DIR/$FILENAME-v$VER.zip" * || ui_error "Failed compression"
+cd "$BASEDIR/sources" || ui_error 'Failed to change folder'
+zip -r9X "$OUT_DIR/$FILENAME-v$VER.zip" * || ui_error 'Failed compression'
 
 # Cleanup remnants
-rm -f "$BASEDIR/sources/"LICENSE* || ui_error "Failed to cleanup"
+rm -f "$BASEDIR/sources/"LICENSE* || ui_error 'Failed to cleanup'
