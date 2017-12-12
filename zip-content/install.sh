a8ff5db3455935323069b205dbe1d256c6d62700
ale5000 <ale5000-git@users.noreply.github.com>
2017-12-13 00:30:30 +0100

    Updated microG Service Core to 0.2.4-107

--- zip-content/install.sh
+++ zip-content/install.sh
@@ -150,7 +150,7 @@ set_perm 0 0 0755 "$TMP_PATH/addon.d/00-1-microg.sh"
 
 # Verifying
 ui_msg_sameline_start 'Verifying...'
-if verify_sha1 "$TMP_PATH/files/priv-app/GmsCore.apk" '90dbb655885d9530997a761c5001fd7f785f972e' &&
+if verify_sha1 "$TMP_PATH/files/priv-app/GmsCore.apk" '8dcc9a0097d8e10478fff6e8c33b534928ba3266' &&
    verify_sha1 "$TMP_PATH/files/priv-app-kk/GmsCore.apk" '52890ef5d04abd563fa220725708fd2bc48e894e' &&  # ToDO: Remove when bug #379 is fixed
    verify_sha1 "$TMP_PATH/files/priv-app/GoogleServicesFramework.apk" 'f9907df2e2c8fd20cd2e928821641fa01fca09ce' &&
    verify_sha1 "$TMP_PATH/files/priv-app/DroidGuard.apk" 'fa6267bee3f73d248d1110be53d66736aa4fece0' &&
