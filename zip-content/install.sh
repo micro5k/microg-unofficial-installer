1786bcc02f951c79d9efa4d696fc1b9fd2ab4b89
ale5000 <ale5000-git@users.noreply.github.com>
2018-01-08 00:28:19 +0100

    Updated NewPipe to 0.11.2

--- zip-content/install.sh
+++ zip-content/install.sh
@@ -154,7 +154,7 @@ if verify_sha1 "$TMP_PATH/files/priv-app/GmsCore.apk" 'c65b108846a40c6493d94117b
    verify_sha1 "$TMP_PATH/files/priv-app-kk/GmsCore.apk" '52890ef5d04abd563fa220725708fd2bc48e894e' &&  # ToDO: Remove when bug #379 is fixed
    verify_sha1 "$TMP_PATH/files/priv-app/GoogleServicesFramework.apk" 'f9907df2e2c8fd20cd2e928821641fa01fca09ce' &&
    verify_sha1 "$TMP_PATH/files/priv-app/DroidGuard.apk" 'fa6267bee3f73d248d1110be53d66736aa4fece0' &&
-   verify_sha1 "$TMP_PATH/files/app/NewPipe.apk" 'dbd14fd60bc7bedeb35fcd8151e91031b5216b0d' &&
+   verify_sha1 "$TMP_PATH/files/app/NewPipe.apk" '518e4517e911dbee06c7ba6beb894d65720189dc' &&
    verify_sha1 "$TMP_PATH/files/app/DejaVuBackend.apk" 'ea098a50b5ac890de54937aecf75eb568c69a674' &&
    verify_sha1 "$TMP_PATH/files/app/IchnaeaNlpBackend.apk" '9be5de681ebb1184fc5e00933f1fd18c080f7ee8' &&
    verify_sha1 "$TMP_PATH/files/app/NominatimGeocoderBackend.apk" '40b0917e9805cdab5abc53925f8732bff9ba8d84' &&
