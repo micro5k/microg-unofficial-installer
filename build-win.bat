3c2694063288c77a9c82b52a17d867f28dc4c923
ale5000 <ale5000-git@users.noreply.github.com>
2018-03-16 23:21:31 +0100

    Fixed executing the batch file from a different folder or as admin

--- build-win.bat
+++ build-win.bat
@@ -3,7 +3,7 @@
 SETLOCAL 2> nul
 TITLE Building the flashable ZIP... 2> nul
 
-"%CD%\tools\win\busybox.exe" bash "%CD%\build.sh"
+"%~dp0tools\win\busybox.exe" bash "%~dp0build.sh"
 
 TITLE Done 2> nul
 ENDLOCAL 2> nul
