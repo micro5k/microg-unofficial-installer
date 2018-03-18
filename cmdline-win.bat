df279790548fd7cc9530d4b0d27a805bf5314994
ale5000 <ale5000-git@users.noreply.github.com>
2018-03-18 12:18:20 +0100

    Setup some alias for convenience

--- cmdline-win.bat
+++ cmdline-win.bat
@@ -7,7 +7,7 @@ IF EXIST "%ProgramFiles(x86)%\GnuWin32\bin" SET PATH=%ProgramFiles(x86)%\GnuWin3
 IF EXIST "%ProgramFiles%\GnuWin32\bin" SET PATH=%ProgramFiles%\GnuWin32\bin;%PATH%
 IF EXIST "%~dp0tools\win" SET PATH=%~dp0tools\win;%PATH%
 
-"%~dp0tools\win\busybox.exe" bash
+"%~dp0tools\win\busybox.exe" ash -s -c "alias dir=ls; alias 'cd..'='cd ..'; alias 'cd.'='cd .'"
 
 TITLE 2> nul
 ENDLOCAL 2> nul
