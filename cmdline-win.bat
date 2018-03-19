a960576df401a6a3902c605f143ee896f82d0d31
ale5000 <ale5000-git@users.noreply.github.com>
2018-03-19 23:14:28 +0100

    Minor changes

--- cmdline-win.bat
+++ cmdline-win.bat
@@ -3,11 +3,11 @@
 SETLOCAL 2> nul
 TITLE Command-line 2> nul
 
-IF EXIST "%ProgramFiles(x86)%\GnuWin32\bin" SET PATH=%ProgramFiles(x86)%\GnuWin32\bin;%PATH%
-IF EXIST "%ProgramFiles%\GnuWin32\bin" SET PATH=%ProgramFiles%\GnuWin32\bin;%PATH%
-IF EXIST "%~dp0tools\win" SET PATH=%~dp0tools\win;%PATH%
+IF EXIST "%ProgramFiles(x86)%\GnuWin32\bin" SET "PATH=%ProgramFiles(x86)%\GnuWin32\bin;%PATH%"
+IF EXIST "%ProgramFiles%\GnuWin32\bin" SET "PATH=%ProgramFiles%\GnuWin32\bin;%PATH%"
+IF EXIST "%~dp0tools\win" SET "PATH=%~dp0tools\win;%PATH%"
 
-"%~dp0tools\win\busybox.exe" ash -s -c "alias dir=ls; alias 'cd..'='cd ..'; alias 'cd.'='cd .'"
+"%~dp0tools\win\busybox.exe" ash -s -c "alias dir=ls; alias 'cd..'='cd ..'; alias 'cd.'='cd .'; alias cls=clear"
 
 TITLE 2> nul
 ENDLOCAL 2> nul
