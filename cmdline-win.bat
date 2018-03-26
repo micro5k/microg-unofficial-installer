0a2c9a0414d3d19e8b8151aba03097dac7a21980
ale5000 <ale5000-git@users.noreply.github.com>
2018-03-26 03:37:12 +0200

    Minor fix

--- cmdline-win.bat
+++ cmdline-win.bat
@@ -8,9 +8,10 @@ IF "%CD%" == "%windir%\system32" CD /D "%~dp0"
 
 IF EXIST "%ProgramFiles(x86)%\GnuWin32\bin" SET "PATH=%ProgramFiles(x86)%\GnuWin32\bin;%PATH%"
 IF EXIST "%ProgramFiles%\GnuWin32\bin" SET "PATH=%ProgramFiles%\GnuWin32\bin;%PATH%"
+SET "PATH=.;%PATH%"
 IF EXIST "%~dp0tools\win" SET "PATH=%~dp0tools\win;%PATH%"
 
-"%~dp0tools\win\busybox.exe" ash -s -c "alias dir=ls; alias 'cd..'='cd ..'; alias 'cd.'='cd .'; alias cls=clear"
+"%~dp0tools\win\busybox.exe" ash -s -c "alias dir=ls; alias 'cd..'='cd ..'; alias 'cd.'='cd .'; alias cls=clear;"
 
 TITLE 2> nul
 ENDLOCAL 2> nul
