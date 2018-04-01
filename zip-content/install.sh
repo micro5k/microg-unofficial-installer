4c9dc18af589fff3356b4edec6b8ed5f2364e573
ale5000 <ale5000-git@users.noreply.github.com>
2018-04-01 12:09:36 +0200

    Bump version

--- zip-content/install.sh
+++ zip-content/install.sh
@@ -112,7 +112,7 @@ fi
 ui_msg ''
 ui_msg '---------------------------'
 ui_msg 'microG unofficial installer'
-ui_msg 'v1.0.27-beta'
+ui_msg 'v1.0.28-alpha'
 ui_msg '(by ale5000)'
 ui_msg '---------------------------'
 ui_msg ''
