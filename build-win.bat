7ba1f879a9a255a977851accd4776490c89c7ddb
ale5000 <ale5000-git@users.noreply.github.com>
2018-11-18 19:50:32 +0100

    Pass back exit code to parent

--- build-win.bat
+++ build-win.bat
@@ -2,11 +2,14 @@
 REM Copyright (C) 2017-2018 ale5000
 REM SPDX-License-Identifer: GPL-3.0-or-later
 
-SETLOCAL 2> nul
 TITLE Building the flashable ZIP... 2> nul
 
+SETLOCAL 2> nul
 "%~dp0tools\win\busybox.exe" bash "%~dp0build.sh"
+ENDLOCAL 2> nul
+SET "EXIT_CODE=%ERRORLEVEL%"
 
 TITLE Done 2> nul
-ENDLOCAL 2> nul
+
 PAUSE > nul
+IF %EXIT_CODE% NEQ 0 EXIT /B %EXIT_CODE%
