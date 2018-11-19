8ab215bcba3cc220034c1b841f10053ca711ec85
ale5000 <ale5000-git@users.noreply.github.com>
2018-11-19 21:52:24 +0100

    Minor changes

--- build-win.bat
+++ build-win.bat
@@ -2,7 +2,8 @@
 REM Copyright (C) 2017-2018 ale5000
 REM SPDX-License-Identifer: GPL-3.0-or-later
 
-TITLE Building the flashable ZIP... 2> nul
+SET "INITIAL_TITLE=%COMSPEC%"
+TITLE Building the flashable OTA zip... 2> nul
 
 SETLOCAL 2> nul
 "%~dp0tools\win\busybox.exe" bash "%~dp0build.sh"
@@ -11,5 +12,10 @@ SET "EXIT_CODE=%ERRORLEVEL%"
 
 TITLE Done 2> nul
 
-PAUSE > nul
+IF NOT "%~1" == "Gradle" PAUSE > nul
+
+TITLE %INITIAL_TITLE% 2> nul
+SET "INITIAL_TITLE="
+
 IF %EXIT_CODE% NEQ 0 EXIT /B %EXIT_CODE%
+SET "EXIT_CODE="
