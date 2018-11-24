356b4740a376df4c62c093fe7920fea2af91a68e
ale5000 <ale5000-git@users.noreply.github.com>
2018-11-24 03:42:21 +0100

    Minor change

--- build.sh
+++ build.sh
@@ -104,7 +104,7 @@ OUT_DIR="$BASEDIR/output"
 mkdir -p "$OUT_DIR" || ui_error 'Failed to create the output dir'
 
 # Create the temp dir
-TEMP_DIR=$(mktemp -d -t ZIPBUILDER-XXXXXXXX)
+TEMP_DIR=$(mktemp -d -t ZIPBUILDER-XXXXXX)
 
 # Set filename and version
 VER=$(cat "$BASEDIR/zip-content/inc/VERSION")
