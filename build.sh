e2fd8449ba039223e9829de2e8ba6f9a0237c316
ale5000 <ale5000-git@users.noreply.github.com>
2019-01-03 22:19:55 +0100

    Unified attributes of files between OSes

--- build.sh
+++ build.sh
@@ -154,7 +154,11 @@ else
 fi
 
 # Useful for reproducible builds
-find "$TEMP_DIR/zip-content/" -exec touch -c -t 197911300100.00 '{}' + || ui_error 'Failed to set modification date'
+find "$TEMP_DIR/zip-content" -exec touch -c -t 200802290333.46 '{}' + || ui_error 'Failed to set modification date of files'
+find "$TEMP_DIR/zip-content" -type d -exec chmod 0700 '{}' + -o -type f -exec chmod 0600 '{}' + || ui_error 'Failed to set permissions of files'
+if test "$PLATFORM" == 'win'; then
+  ATTRIB -R -A -S -H "$TEMP_DIR/zip-content/*" /S /D
+fi
 
 # Remove the previously built files (if they exist)
 rm -f "$OUT_DIR/${FILENAME}".zip* || ui_error 'Failed to remove the previously built files'
