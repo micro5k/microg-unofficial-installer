dc4196656392a07d1edea7012a9d2a50d4aee25a
ale5000 <ale5000-git@users.noreply.github.com>
2019-02-05 11:33:33 +0100

    Disable variable expansion in here documents

--- conf.sh
+++ conf.sh
@@ -4,13 +4,13 @@ export NAME='microG-unofficial-installer-ale5000'
 
 oss_files_to_download()
 {
-cat <<EOF
+cat <<'EOF'
 EOF
 }
 
 files_to_download()
 {
-cat <<EOF
+cat <<'EOF'
 PlayStore-recent.apk|files/variants|6c60fa863dd7befef49082c0dcf6278947a09333|https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=137220
 PlayStore-legacy.apk|files/variants|d78b377db43a2bc0570f37b2dd0efa4ec0b95746|https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=2911
 EOF
