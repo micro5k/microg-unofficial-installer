608ab8f9bd0fc06fe2b967046f3685f03944ebca
ale5000 <ale5000-git@users.noreply.github.com>
2019-03-18 17:52:59 +0100

    Remove remnant code

--- build-win.bat
+++ build-win.bat
@@ -2,13 +2,11 @@
 REM Copyright (C) 2017-2018 ale5000
 REM SPDX-License-Identifer: GPL-3.0-or-later
 
-TITLE Building the flashable OTA zip... 2> nul
 SETLOCAL 2> nul
 CHCP 858 >nul || ECHO "Changing the codepage failed"
 "%~dp0tools\win\busybox.exe" ash "%~dp0build.sh"
 ENDLOCAL 2> nul
 SET "EXIT_CODE=%ERRORLEVEL%"
-TITLE Done 2> nul
 
 IF NOT "%~1" == "Gradle" PAUSE > nul
 
