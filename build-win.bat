68c4d69f4cb4babe5824a3716fd74bb96259b8d0
ale5000 <ale5000-git@users.noreply.github.com>
2019-03-22 20:30:43 +0100

    Pass parameters to the shell/script

--- build-win.bat
+++ build-win.bat
@@ -4,7 +4,7 @@ REM SPDX-License-Identifer: GPL-3.0-or-later
 
 SETLOCAL 2> nul
 CHCP 858 >nul || ECHO "Changing the codepage failed"
-"%~dp0tools\win\busybox.exe" ash "%~dp0build.sh"
+"%~dp0tools\win\busybox.exe" ash "%~dp0build.sh" %*
 ENDLOCAL 2> nul
 SET "EXIT_CODE=%ERRORLEVEL%"
 
