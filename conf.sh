d9178cea7009714bd512c245cff34b1bb869f51e
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2021-09-21 17:03:37 +0200

    Update conf.sh

--- conf.sh
+++ conf.sh
@@ -11,7 +11,7 @@ EOF
 files_to_download()
 {
 cat <<'EOF'
-PlayStore-recent.apk|files/variants|6c60fa863dd7befef49082c0dcf6278947a09333|https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=137220
-PlayStore-legacy.apk|files/variants|d78b377db43a2bc0570f37b2dd0efa4ec0b95746|https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=2911
+PlayStore-recent.apk|files/variants|6c60fa863dd7befef49082c0dcf6278947a09333|https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=137220|https://www.apkmirror.com/apk/google-inc/google-play-store/google-play-store-7-1-25-release/google-play-store-7-1-25-i-all-0-pr-137772785-android-apk-download/download/
+PlayStore-legacy.apk|files/variants|d78b377db43a2bc0570f37b2dd0efa4ec0b95746|https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=2911|
 EOF
 }
