812dfbe925e096f7d4871683e066d2ea582dcf02
ale5000 <ale5000-git@users.noreply.github.com>
2021-09-21 19:20:20 +0200

    Add mirror links

--- conf.sh
+++ conf.sh
@@ -11,7 +11,7 @@ EOF
 files_to_download()
 {
 cat <<'EOF'
-PlayStore-recent.apk|files/variants|6c60fa863dd7befef49082c0dcf6278947a09333|https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=137220|https://www.apkmirror.com/apk/google-inc/google-play-store/google-play-store-7-1-25-release/google-play-store-7-1-25-i-all-0-pr-137772785-android-apk-download/download/
-PlayStore-legacy.apk|files/variants|d78b377db43a2bc0570f37b2dd0efa4ec0b95746|https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=2911|
+PlayStore-recent.apk|files/variants|6c60fa863dd7befef49082c0dcf6278947a09333|https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=137220|https://mpl1.androidfilehost.com/dl/7NbnmhTeNneSVqH9GOI-Xw/1632331102/7161016148664811887/PlayStore-recent.apk|https://www.apkmirror.com/apk/google-inc/google-play-store/google-play-store-7-1-25-release/google-play-store-7-1-25-i-all-0-pr-137772785-android-apk-download/download/
+PlayStore-legacy.apk|files/variants|d78b377db43a2bc0570f37b2dd0efa4ec0b95746|https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=2911|https://mde1.androidfilehost.com/dl/VHog4WlGNQ7i9cu0Ur16BA/1632331005/7161016148664811888/PlayStore-legacy.apk|
 EOF
 }
