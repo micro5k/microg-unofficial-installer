f0e70801aa917fe9d4c2c2aca0c0318be8ffbfc1
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2021-11-18 21:47:47 +0100

    Change license of some settings files to CC0-1.0

--- conf.sh
+++ conf.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# SPDX-FileCopyrightText: Copyright (C) 2016-2019, 2021 ale5000
+# SPDX-License-Identifer: CC0-1.0
+# SPDX-FileType: SOURCE
+
 export NAME='microG-unofficial-installer-ale5000'
 
 oss_files_to_download()
