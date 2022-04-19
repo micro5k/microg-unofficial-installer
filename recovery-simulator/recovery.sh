bb65986d7545004c8b78d1af8f804d727a37ecb6
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2022-04-19 21:18:25 +0200

    Fix typo

--- recovery-simulator/recovery.sh
+++ recovery-simulator/recovery.sh
@@ -7,7 +7,7 @@
 # NOTE: This script simulate a real recovery but it relies on the zip to use the suggested paths.
 # IMPORTANT: A misbehaving zip can damage your real system.
 
-fail_wih_msg()
+fail_with_msg()
 {
   echo "${1}"
   exit 1
@@ -15,24 +15,24 @@ fail_wih_msg()
 
 # Reset environment
 if ! "${ENV_RESETTED:-false}"; then
-  THIS_SCRIPT="$(realpath "${0}" 2>&-)" || fail_wih_msg 'Failed to get script filename'
+  THIS_SCRIPT="$(realpath "${0}" 2>&-)" || fail_with_msg 'Failed to get script filename'
   # Create the temp dir (must be done before resetting environment)
-  OUR_TEMP_DIR="$(mktemp -d -t ANDR-RECOV-XXXXXX)" || fail_wih_msg 'Failed to create our temp dir'
+  OUR_TEMP_DIR="$(mktemp -d -t ANDR-RECOV-XXXXXX)" || fail_with_msg 'Failed to create our temp dir'
   exec env -i ENV_RESETTED=true THIS_SCRIPT="${THIS_SCRIPT}" OUR_TEMP_DIR="${OUR_TEMP_DIR}" PATH="${PATH}" bash "${THIS_SCRIPT}" "${1}"
 fi
 unset ENV_RESETTED
 
 # Check dependencies
-UNZIP_CMD="$(command -v unzip)" || fail_wih_msg 'Unzip is missing'
-CUSTOM_BUSYBOX="$(command -v busybox)" || fail_wih_msg 'BusyBox is missing'
+UNZIP_CMD="$(command -v unzip)" || fail_with_msg 'Unzip is missing'
+CUSTOM_BUSYBOX="$(command -v busybox)" || fail_with_msg 'BusyBox is missing'
 
 # Get dir of this script
-THIS_SCRIPT_DIR="$(dirname "${THIS_SCRIPT}")" || fail_wih_msg 'Failed to get script dir'
+THIS_SCRIPT_DIR="$(dirname "${THIS_SCRIPT}")" || fail_with_msg 'Failed to get script dir'
 unset THIS_SCRIPT
 
 # Ensure we have a path the the temp dir and empty it (should be already empty, but we must be sure)
-if test -z "${OUR_TEMP_DIR}"; then fail_wih_msg 'Failed to create our temp dir'; fi
-rm -rf "${OUR_TEMP_DIR:?}"/* || fail_wih_msg 'Failed to empty our temp dir'
+if test -z "${OUR_TEMP_DIR}"; then fail_with_msg 'Failed to create our temp dir'; fi
+rm -rf "${OUR_TEMP_DIR:?}"/* || fail_with_msg 'Failed to empty our temp dir'
 
 # Simulate the environment variables (part 1)
 BASE_SIMULATION_PATH="${OUR_TEMP_DIR}/root"  # Internal var
@@ -64,8 +64,8 @@ ln -s "${EXTERNAL_STORAGE}" "${BASE_SIMULATION_PATH}/sdcard" || mkdir -p "${BASE
 } > "${ANDROID_ROOT}/build.prop"
 
 mkdir -p "${TMPDIR}"
-cp -rf "${THIS_SCRIPT_DIR}/updater" "${TMPDIR}/updater" || fail_wih_msg 'Failed to copy the updater script'
-chmod +x "${TMPDIR}/updater" || fail_wih_msg "chmod failed on '${TMPDIR}/updater'"
+cp -rf "${THIS_SCRIPT_DIR}/updater" "${TMPDIR}/updater" || fail_with_msg 'Failed to copy the updater script'
+chmod +x "${TMPDIR}/updater" || fail_with_msg "chmod failed on '${TMPDIR}/updater'"
 
 mkdir -p "${THIS_SCRIPT_DIR}/output"
 touch "${THIS_SCRIPT_DIR}/output/recovery_output"
@@ -86,15 +86,15 @@ export CUSTOM_BUSYBOX
 
 # Prepare before execution
 FLASHABLE_ZIP_PATH="${1}"
-FLASHABLE_ZIP="$("${CUSTOM_BUSYBOX}" basename "${FLASHABLE_ZIP_PATH}")" || fail_wih_msg 'Failed to get the dir of the flashable ZIP'
-"${CUSTOM_BUSYBOX}" cp -rf "${FLASHABLE_ZIP_PATH}" "${SECONDARY_STORAGE}/${FLASHABLE_ZIP}" || fail_wih_msg 'Failed to copy the flashable ZIP'
-"${UNZIP_CMD}" -opq "${SECONDARY_STORAGE}/${FLASHABLE_ZIP}" 'META-INF/com/google/android/update-binary' > "${TMPDIR}/update-binary" || fail_wih_msg 'Failed to extract the update-binary'
+FLASHABLE_ZIP="$("${CUSTOM_BUSYBOX}" basename "${FLASHABLE_ZIP_PATH}")" || fail_with_msg 'Failed to get the dir of the flashable ZIP'
+"${CUSTOM_BUSYBOX}" cp -rf "${FLASHABLE_ZIP_PATH}" "${SECONDARY_STORAGE}/${FLASHABLE_ZIP}" || fail_with_msg 'Failed to copy the flashable ZIP'
+"${UNZIP_CMD}" -opq "${SECONDARY_STORAGE}/${FLASHABLE_ZIP}" 'META-INF/com/google/android/update-binary' > "${TMPDIR}/update-binary" || fail_with_msg 'Failed to extract the update-binary'
 
 # Execute the script that will run the flashable zip
-cd "${BASE_SIMULATION_PATH}" || fail_wih_msg 'Failed to change dir to the base simulation path'
+cd "${BASE_SIMULATION_PATH}" || fail_with_msg 'Failed to change dir to the base simulation path'
 "${CUSTOM_BUSYBOX}" sh "${TMPDIR}/updater" 3 "${RECOVERY_FD}" "${SECONDARY_STORAGE}/${FLASHABLE_ZIP}"; STATUS="$?"
 rm -r "${TMPDIR}/update-binary"
 
 unset TMPDIR
 rm -rf "${OUR_TEMP_DIR:?}" &
-if test "${STATUS}" -ne 0; then fail_wih_msg "Installation failed with error ${STATUS}"; fi
+if test "${STATUS}" -ne 0; then fail_with_msg "Installation failed with error ${STATUS}"; fi
