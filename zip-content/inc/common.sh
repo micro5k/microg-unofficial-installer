71275a4db083c1a6033db2b33079e3d2bcff20c1
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2022-04-26 23:00:07 +0200

    Further improve is_mounted

--- zip-content/inc/common.sh
+++ zip-content/inc/common.sh
@@ -99,7 +99,7 @@ is_mounted()
 {
   local _partition _mount_result
   _partition="$(readlink -f "${1}")" || { _partition="${1}"; ui_warning "Failed to canonicalize '${1}'"; }
-  _mount_result="$(mount)" || _mount_result="$(cat /proc/mounts)" || ui_error 'is_mounted has failed'
+  _mount_result="$(mount)" || test -e '/proc/mounts' && _mount_result="$(cat /proc/mounts)" || test -n "${DEVICE_MOUNT:-}" && _mount_result="$("${DEVICE_MOUNT:-}")" || ui_error 'is_mounted has failed'
 
   case "${_mount_result}" in
     *[[:blank:]]"${_partition}"[[:blank:]]*) return 0;;  # Mounted
