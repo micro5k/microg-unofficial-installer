41a6ab6183ceab41b1d5ad6ec2e0e83066311572
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2022-05-17 15:33:30 +0200

    Improve is_mounted

--- zip-content/META-INF/com/google/android/update-binary.sh
+++ zip-content/META-INF/com/google/android/update-binary.sh
@@ -88,11 +88,11 @@ ui_debug()
 is_mounted()
 {
   local _partition _mount_result
-  _partition="${1:?}"
+  _partition="$(readlink -f "${1:?}")" || { _partition="${1:?}"; ui_warning "Failed to canonicalize '${1}'"; }
   _mount_result="$(mount)" || { test -e '/proc/mounts' && _mount_result="$(cat /proc/mounts)"; } || ui_error 'is_mounted has failed'
 
-  case "${_mount_result}" in
-    *[[:blank:]]"${_partition}"[[:blank:]]*) return 0;;  # Mounted
+  case "${_mount_result:?}" in
+    *[[:blank:]]"${_partition:?}"[[:blank:]]*) return 0;;  # Mounted
     *)                                                   # NOT mounted
   esac
   return 1  # NOT mounted
