f4da44ee764229acc029c0a04984f2b7c0f0bf30
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2022-05-19 17:06:06 +0200

    Minor change

--- zip-content/META-INF/com/google/android/update-binary.sh
+++ zip-content/META-INF/com/google/android/update-binary.sh
@@ -332,7 +332,9 @@ if test -e "${BASE_TMP_PATH}/keycheck"; then
   KEYCHECK_ENABLED=true
 fi
 
-if test "${CI:-false}" != 'false'; then
+# Live setup isn't supported under continuous integration system
+# Live setup doesn't work when executed through Gradle
+if test "${CI:-false}" != 'false' || test "${APP_BASE_NAME:-false}" = 'gradlew' || test "${APP_NAME:-false}" = 'Gradle'; then
   LIVE_SETUP_POSSIBLE=false
   LIVE_SETUP=0
 fi
