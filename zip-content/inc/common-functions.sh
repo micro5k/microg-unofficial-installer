603f000616c268c9a355d3ae89053a6102eab29e
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2022-05-19 09:53:53 +0200

    Minor changes

--- zip-content/inc/common-functions.sh
+++ zip-content/inc/common-functions.sh
@@ -100,12 +100,12 @@ mount_partition()
 is_mounted()
 {
   local _partition _mount_result
-  _partition="$(readlink -f "${1}")" || { _partition="${1}"; ui_warning "Failed to canonicalize '${1}'"; }
-  _mount_result="$(mount)" || { test -e '/proc/mounts' && _mount_result="$(cat /proc/mounts)"; } || { test -n "${DEVICE_MOUNT:-}" && _mount_result="$("${DEVICE_MOUNT:-}")"; } || ui_error 'is_mounted has failed'
+  _partition="$(readlink -f "${1:?}")" || { _partition="${1:?}"; ui_warning "Failed to canonicalize '${1}'"; }
+  _mount_result="$(mount)" || { test -e '/proc/mounts' && _mount_result="$(cat /proc/mounts)"; } || { test -n "${DEVICE_MOUNT:-}" && _mount_result="$("${DEVICE_MOUNT:?}")"; } || ui_error 'is_mounted has failed'
 
-  case "${_mount_result}" in
-    *[[:blank:]]"${_partition}"[[:blank:]]*) return 0;;  # Mounted
-    *)                                                   # NOT mounted
+  case "${_mount_result:?}" in
+    *[[:blank:]]"${_partition:?}"[[:blank:]]*) return 0;;  # Mounted
+    *)                                                     # NOT mounted
   esac
   return 1  # NOT mounted
 }
