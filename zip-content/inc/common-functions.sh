bac5543c55884ab9e6f32157051f0bdcd830352d
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2022-05-23 13:48:04 +0200

    Minor changes

--- zip-content/inc/common-functions.sh
+++ zip-content/inc/common-functions.sh
@@ -20,61 +20,57 @@ fi
 # Message related functions
 _show_text_on_recovery()
 {
-  if test -e "${RECOVERY_PIPE}"; then
-    printf "ui_print %s\nui_print \n" "${1}" >> "${RECOVERY_PIPE}"
+  if test -e "${RECOVERY_PIPE:?}"; then
+    printf "ui_print %s\nui_print \n" "${1?}" >> "${RECOVERY_PIPE:?}"
   else
-    printf "ui_print %s\nui_print \n" "${1}" 1>&"${OUTFD}"
+    printf "ui_print %s\nui_print \n" "${1?}" 1>&"${OUTFD:?}"
   fi
 }
 
 ui_error()
 {
   ERROR_CODE=91
-  if test -n "$2"; then ERROR_CODE="$2"; fi
-  >&2 echo "ERROR ${ERROR_CODE}: $1"
-  _show_text_on_recovery "ERROR: $1"
-  exit "${ERROR_CODE}"
+  if test -n "${2}"; then ERROR_CODE="${2:?}"; fi
+  1>&2 echo "ERROR ${ERROR_CODE:?}: ${1?}"
+  _show_text_on_recovery "ERROR: ${1?}"
+  exit "${ERROR_CODE:?}"
 }
 
 ui_warning()
 {
-  >&2 echo "WARNING: $1"
-  _show_text_on_recovery "WARNING: $1"
+  1>&2 echo "WARNING: ${1?}"
+  _show_text_on_recovery "WARNING: ${1?}"
 }
 
 ui_msg()
 {
-  if test "${DEBUG_LOG}" -ne 0; then echo "$1"; fi
-  if test -e "${RECOVERY_PIPE}"; then
-    printf "ui_print %s\nui_print \n" "${1}" >> "${RECOVERY_PIPE}"
-  else
-    printf "ui_print %s\nui_print \n" "${1}" 1>&"${OUTFD}"
-  fi
+  if test "${DEBUG_LOG}" -ne 0; then echo "${1?}"; fi
+  _show_text_on_recovery "${1?}"
 }
 
 ui_msg_sameline_start()
 {
-  if test "${DEBUG_LOG}" -ne 0; then printf '%s\n' "${1}"; fi
+  if test "${DEBUG_LOG}" -ne 0; then printf '%s\n' "${1?}"; fi
   if test -e "${RECOVERY_PIPE}"; then
-    printf 'ui_print %s' "${1}" >> "${RECOVERY_PIPE}"
+    printf 'ui_print %s' "${1?}" >> "${RECOVERY_PIPE:?}"
   else
-    printf 'ui_print %s' "${1}" 1>&"${OUTFD}"
+    printf 'ui_print %s' "${1?}" 1>&"${OUTFD:?}"
   fi
 }
 
 ui_msg_sameline_end()
 {
-  if test "${DEBUG_LOG}" -ne 0; then printf '%s\n' "${1}"; fi
+  if test "${DEBUG_LOG}" -ne 0; then printf '%s\n' "${1?}"; fi
   if test -e "${RECOVERY_PIPE}"; then
-    printf '%s\nui_print \n' "${1}" >> "${RECOVERY_PIPE}"
+    printf '%s\nui_print \n' "${1?}" >> "${RECOVERY_PIPE:?}"
   else
-    printf '%s\nui_print \n' "${1}" 1>&"${OUTFD}"
+    printf '%s\nui_print \n' "${1?}" 1>&"${OUTFD:?}"
   fi
 }
 
 ui_debug()
 {
-  echo "$1"
+  echo "${1?}"
 }
 
 # Error checking functions
