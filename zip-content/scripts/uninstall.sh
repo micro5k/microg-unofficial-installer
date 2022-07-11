c04467463f457104f87d60491c46562f25fc0f2c
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2022-07-11 16:01:05 +0200

    Minor changes

--- zip-content/scripts/uninstall.sh
+++ zip-content/scripts/uninstall.sh
@@ -55,6 +55,7 @@ GsfProxy|
 PrebuiltGmsCore|
 PrebuiltGmsCorePi|
 PrebuiltGmsCorePix|
+PrebuiltGmsCoreSc|
 MicroGGMSCore|
 
 MarketUpdater|com.android.vending.updater
@@ -238,6 +239,7 @@ delete_recursive_wildcard "${SYS_PATH}"/etc/default-permissions/microg-*-permiss
 
 delete_recursive "${SYS_PATH}"/etc/permissions/features.xml
 delete_recursive "${SYS_PATH}"/etc/permissions/privapp-permissions-google.xml
+delete_recursive "${SYS_PATH}"/etc/permissions/privapp-permissions-google-p.xml
 delete_recursive "${SYS_PATH}"/etc/permissions/privapp-permissions-microg.xml
 
 delete_recursive "${SYS_PATH}"/etc/sysconfig/features.xml
