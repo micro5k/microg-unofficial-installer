7638ad589812164fb8947f582f4cb1cd3bbfe945
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2022-07-12 17:15:03 +0200

    Minor fix

--- recovery-simulator/inc/configure-overrides.sh
+++ recovery-simulator/inc/configure-overrides.sh
@@ -11,7 +11,7 @@ fi
 rs_function_exists()
 {
   # shellcheck disable=SC2312
-  LC_ALL=C type -- "${1:?}" 2>/dev/null | grep -Fq -- 'is a function'
+  LC_ALL=C type "${1:?}" 2>/dev/null | grep -Fq -- 'is a function'
   return "${?}"
 }
 
