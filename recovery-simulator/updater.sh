41da93299a4a376f0a1e12f1636689580001d8b1
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2022-07-12 16:09:27 +0200

    Use the new override script

--- recovery-simulator/updater.sh
+++ recovery-simulator/updater.sh
@@ -3,16 +3,11 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # SPDX-FileType: SOURCE
 
-function_exists()
-{
-  LC_ALL=C type "${1:?}" 2>/dev/null | grep -Fq -- 'is a function'
-}
+# Ensure that the overridden commands are preferred over BusyBox applets (and that unsafe commands aren't accessible)
+export BB_OVERRIDE_APPLETS='mount umount chown su sudo' || exit 125
 
-override_command_fallback()
-{
-  # shellcheck disable=SC2139
-  alias "${1:?}='${OVERRIDE_DIR:?}/${1:?}'" || return "${?}"  # This expands when defined, not when used
-}
+# shellcheck source=SCRIPTDIR/inc/configure-overrides.sh
+. "${RS_OVERRIDE_SCRIPT:?}" || exit "${?}"
 
 unset OUR_TEMP_DIR
 unset FUNCNAME
@@ -30,18 +25,6 @@ PS2='> '
 if test "${COVERAGE:-false}" = 'false'; then
   PS4='+ '
 fi
-
-# Ensure that the overridden commands are preferred over BusyBox applets (and that unsafe commands aren't accessible)
-export BB_OVERRIDE_APPLETS='su sudo mount umount chown' || exit 125
-override_command_fallback mount || exit 124
-override_command_fallback umount || exit 124
-override_command_fallback chown || exit 124
-override_command_fallback su || exit 124
-override_command_fallback sudo || exit 124
-
-unset -f function_exists
-unset -f override_command_fallback
-unset OVERRIDE_DIR
 
 # shellcheck source=SCRIPTDIR/../zip-content/META-INF/com/google/android/update-binary.sh
 . "${TMPDIR:?}/update-binary" || exit "${?}"
