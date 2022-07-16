6d201230dc7c5da5422bc5b029b9ecc019432f92
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2022-07-16 13:12:18 +0200

    Added utility to remove all accounts from the device

--- zip-content/files/bin/minutil.sh
+++ zip-content/files/bin/minutil.sh
@@ -19,6 +19,25 @@ _minutil_find_package()
   pm path -- "${1:?}" | cut -d ':' -f 2 || return 1
 }
 
+_list_account_files()
+{
+  cat <<'EOF'
+/data/system_de/0/accounts_de.db
+/data/system_de/0/accounts_de.db-journal
+/data/system_ce/0/accounts_ce.db
+/data/system_ce/0/accounts_ce.db-journal
+/data/system/users/0/accounts.db
+/data/system/users/0/accounts.db-journal
+/data/system/accounts.db
+/data/system/accounts.db-journal
+/data/data/com.google.android.gms/shared_prefs/accounts.xml
+/data/system/sync/stats.bin
+/data/system/sync/pending.xml
+/data/system/sync/accounts.xml
+/data/system/sync/status.bin
+EOF
+}
+
 minutil_reinstall_package()
 {
   # shellcheck disable=2310
@@ -28,16 +47,30 @@ minutil_reinstall_package()
   echo "Package ${1:?} reinstalled."
 }
 
+minutil_remove_all_accounts()
+{
+  # shellcheck disable=2310
+  _list_account_files | while IFS='' read -r _file; do
+    if test -e "${_file:?}"; then
+      echo "Deleting '${_file:?}'..."
+      rm -f -- "${_file}" || return 1
+    fi
+  done || { echo 'ERROR: Failed to delete accounts'; return 4; }
+  echo "All accounts deleted."
+}
+
 
 case "${1}" in
   -i | --reinstall-package )
-    minutil_reinstall_package "${2:?}"
-  ;;
+    minutil_reinstall_package "${2:?}";;
+
+  --remove-all-accounts )
+    minutil_remove_all_accounts;;
 
   * )
-    echo "MinUtil
+    echo 'MinUtil
 Various utility functions.
 
--i | --reinstall-package PACKAGE_NAME			Reinstall package as if it were installed from Play Store and grant it all permissions, example: minutil -i org.schabi.newpipe"
-  ;;
+-i | --reinstall-package PACKAGE_NAME		Reinstall package as if it were installed from Play Store and grant it all permissions, example: minutil -i org.schabi.newpipe
+--remove-all-accounts				Remove all accounts from the device';;
 esac
