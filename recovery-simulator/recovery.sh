1e0c9f19f22bde0c14ad9f97b719bffc1dfccb1a
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2022-08-02 16:33:58 +0200

    Minor change

--- recovery-simulator/recovery.sh
+++ recovery-simulator/recovery.sh
@@ -316,7 +316,7 @@ if test "${uname_o_saved:?}" != 'MS/Windows'; then
 fi
 
 # List installed files
-ls -ARFl --color -- "${BASE_SIMULATION_PATH}" || true
+ls -ARFl --color=always -- "${BASE_SIMULATION_PATH}" || true
 
 parse_recovery_output()
 {
