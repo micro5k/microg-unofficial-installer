6d848122c032e49a8a8ff5741c32c66d1ddcf2d5
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2022-09-07 15:03:50 +0200

    Prepare some functions for refactoring (not ready yet)

--- zip-content/inc/common-functions.sh
+++ zip-content/inc/common-functions.sh
@@ -434,20 +434,31 @@ file_get_first_line_that_start_with()
   grep -m 1 -e "^${1:?}" -- "${2:?}" || return "${?}"
 }
 
+string_split()
+{
+  printf '%s' "${1:?}" | cut -d '|' -sf "${2:?}" || return "${?}"
+}
+
 setup_app()  # $1 => Default setting  $2 => Name  $3 => Filename  $4 => Folder
 {
-  local _install _app_conf
+  local _install _app_conf _min_sdk _max_sdk
   _install="${1:-0}"
-  _app_conf="$(file_get_first_line_that_start_with "${4:?}/${3:?}|" "${TMP_PATH}/files/system-apps/file-list.dat")"
-  # INCOMPLETE
+  _app_conf="$(file_get_first_line_that_start_with "${4:?}/${3:?}|" "${TMP_PATH}/files/system-apps/file-list.dat")" || ui_error "Failed to get app config for '${2}'"
+  _min_sdk="$(string_split "${_app_conf:?}" 2)" || ui_error "Failed to get min SDK for '${2}'"
+  _max_sdk="$(string_split "${_app_conf:?}" 3)" || ui_error "Failed to get max SDK for '${2}'"
+  _output_name="$(string_split "${_app_conf:?}" 4)" || ui_error "Failed to get output name for '${2}'"
 
-  if test "${live_setup_enabled:?}" = 'true'; then
-    choose "Do you want to install ${2:?}?" '+) Yes' '-) No'
-    if test "${?}" -eq 3; then _install='1'; else _install='0'; fi
-  fi
+  if test "${API:?}" -ge "${_min_sdk:?}" && test "${API:?}" -le "${_max_sdk:-99}"; then
+    if test "${live_setup_enabled:?}" = 'true'; then
+      choose "Do you want to install ${2:?}?" '+) Yes' '-) No'
+      if test "${?}" -eq 3; then _install='1'; else _install='0'; fi
+    fi
 
-  if test "${_install:?}" -ne 0; then
-    move_rename_file "${TMP_PATH}/files/system-apps/${4:?}/${3:?}.apk" "${TMP_PATH}/files/${4:?}/${3:?}.apk"
+    if test "${_install:?}" -ne 0; then
+      echo move_rename_file "${TMP_PATH}/files/system-apps/${4:?}/${3:?}.apk" "${TMP_PATH}/files/${4:?}/${_output_name:?}.apk"
+    fi
+  else
+    ui_debug "Skipped: ${2:?}"
   fi
 }
 
