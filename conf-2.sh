f24e79d50899c79e5d64563916cd1ad92bbb933c
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2022-09-27 23:37:07 +0200

    Update Android Auto to 1.2.520120-stub Minor changes

--- conf-2.sh
+++ conf-2.sh
@@ -6,8 +6,8 @@
 files_to_download()
 {
 cat <<'EOF'
-PlayStoreLegacy|priv-app|9|22|Phonesky|com.android.vending|d78b377db43a2bc0570f37b2dd0efa4ec0b95746|www.apkmirror.com/apk/google-inc/google-play-store/google-play-store-5-1-11-3-release/google-play-store-5-1-11-android-apk-download/|store8.gofile.io/download/ffecc71c-733d-47ab-9387-f6db7d6f54ae/PlayStoreLegacy.apk
-PlayStore|priv-app|23||Phonesky|com.android.vending|6c60fa863dd7befef49082c0dcf6278947a09333|www.apkmirror.com/apk/google-inc/google-play-store/google-play-store-7-1-25-release/google-play-store-7-1-25-i-all-0-pr-137772785-android-apk-download/|store8.gofile.io/download/0ddd3127-10a4-4a59-b6b1-6f06076ba75e/PlayStore.apk
-AndroidAuto|priv-app|23||AndroidAuto|com.google.android.projection.gearhead|70ca5318fc24b462f1da045e7639260c63db252e|www.apkmirror.com/apk/google-inc/android-auto/android-auto-1-2-512930-stub-release/android-auto-1-2-512930-stub-android-apk-download/|store8.gofile.io/download/2b340957-7684-43f3-84da-c5d4ad159e57/AndroidAuto.apk
+PlayStoreLegacy|priv-app|9|22|Phonesky|com.android.vending|d78b377db43a2bc0570f37b2dd0efa4ec0b95746|www.apkmirror.com/apk/google-inc/google-play-store/google-play-store-5-1-11-3-release/google-play-store-5-1-11-android-apk-download/|store9.gofile.io/download/f561656b-831a-45f3-b1e1-6102dc9cb25d/PlayStoreLegacy.apk
+PlayStore|priv-app|23||Phonesky|com.android.vending|6c60fa863dd7befef49082c0dcf6278947a09333|www.apkmirror.com/apk/google-inc/google-play-store/google-play-store-7-1-25-release/google-play-store-7-1-25-i-all-0-pr-137772785-android-apk-download/|store9.gofile.io/download/5db8aa94-438e-4620-ba06-016efed153d9/PlayStore.apk
+AndroidAuto|priv-app|23||AndroidAuto|com.google.android.projection.gearhead|1ebc63051e5728030eac615210cbdaa25afdc0cb|www.apkmirror.com/apk/google-inc/android-auto/android-auto-1-2-520120-stub-release/android-auto-1-2-520120-stub-android-apk-download/|store9.gofile.io/download/1e00ce89-ff92-476c-ad1f-dc4853be2688/AndroidAuto.apk
 EOF
 }
