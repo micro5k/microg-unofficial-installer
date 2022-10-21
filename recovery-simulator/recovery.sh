1f17f3ebf57e19a470b4ec1ef41937375cadfb43
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2022-10-21 16:20:51 +0200

    Minor fix

--- recovery-simulator/recovery.sh
+++ recovery-simulator/recovery.sh
@@ -8,9 +8,9 @@
 
 set -e
 # shellcheck disable=SC3040
-set -o posix 2> /dev/null || true
-# shellcheck disable=SC3040
 set -o pipefail || true
+# shellcheck disable=SC3044
+shopt -s inherit_errexit 2> /dev/null || true
 
 # shellcheck disable=SC3028
 case ":${SHELLOPTS:-}:" in
