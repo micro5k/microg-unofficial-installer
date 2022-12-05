fa87fdcb9c56332b29d4a7c0ea6b172a270c1ad0
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2022-12-05 10:47:46 +0100

    Minor fixes

--- zip-content/zip-install.sh
+++ zip-content/zip-install.sh
@@ -3,7 +3,8 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # SPDX-FileType: SOURCE
 
-ZIPFILE="$(realpath ${1:?ERROR: You must specify the ZIP file to install})" || exit 1
+ZIPFILE="$(realpath "${1:?ERROR: You must specify the ZIP file to install}")" || exit 1
+# shellcheck disable=SC3028
 SCRIPT_NAME="./update-binary-${RANDOM:?}.sh" || exit 2
 unzip -pq "${ZIPFILE:?}" 'META-INF/com/google/android/update-binary' 1> "${SCRIPT_NAME:?}" || { printf 'ERROR: %s\n' 'Failed to extract update-binary'; exit 3; }
 sh -- "${SCRIPT_NAME:?}" 3 1 "${ZIPFILE:?}" || { printf 'ERROR: %s\n' 'ZIP installation failed'; exit 4; }
