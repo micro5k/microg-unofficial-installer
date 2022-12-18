c247bf166c17e3780902845b318900328943d1a2
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2022-12-18 18:35:20 +0100

    Improve zip-install

--- zip-content/zip-install.sh
+++ zip-content/zip-install.sh
@@ -3,10 +3,31 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # SPDX-FileType: SOURCE
 
-test -n "${1:-}" || { printf 'ERROR: %s\n' 'You must specify the ZIP file to install'; exit 1; }
+umask 022 || exit 1
+
+test -n "${1:-}" || { printf 'ERROR: %s\n' 'You must specify the ZIP file to install'; exit 2; }
 ZIPFILE="$(realpath -- "${1:?}")" || ZIPFILE="$(readlink -f -- "${1:?}")" || exit 2
-# shellcheck disable=SC3028
-SCRIPT_NAME="./update-binary-${RANDOM:?}.sh" || exit 3
-unzip -pq "${ZIPFILE:?}" 'META-INF/com/google/android/update-binary' 1> "${SCRIPT_NAME:?}" || { printf 'ERROR: %s\n' 'Failed to extract update-binary'; exit 4; }
-sh -- "${SCRIPT_NAME:?}" 3 1 "${ZIPFILE:?}" || { printf 'ERROR: %s\n' 'ZIP installation failed'; exit 5; }
+
+TMPDIR="${TMPDIR:-}"
+if test -n "${TMPDIR:-}" && test -w "${TMPDIR:?}"; then
+  : # Already ready
+elif test -w '/tmp'; then
+  TMPDIR='/tmp'
+elif test -e '/dev'; then
+  mkdir -p -- '/dev/tmp' || { printf 'ERROR: %s\n' 'Failed to create a temp folder'; exit 3; }
+  TMPDIR='/dev/tmp'
+fi
+
+if test -z "${TMPDIR:-}" || test ! -w "${TMPDIR:?}"; then
+  printf 'ERROR: %s\n' 'Unable to create a temp folder'
+  exit 4
+fi
+export TMPDIR
+
+SCRIPT_NAME="${TMPDIR:?}/update-binary.sh" || exit 5
+unzip -pq "${ZIPFILE:?}" 'META-INF/com/google/android/update-binary' 1> "${SCRIPT_NAME:?}" || { printf 'ERROR: %s\n' 'Failed to extract update-binary'; exit 6; }
+sh -- "${SCRIPT_NAME:?}" 3 1 "${ZIPFILE:?}" || { printf 'ERROR: %s\n' 'ZIP installation failed'; exit 7; }
 rm -f -- "${SCRIPT_NAME:?}" || true
+if test "${TMPDIR:?}" = '/dev/tmp'; then
+  rmdir --ignore-fail-on-non-empty -- "${TMPDIR:?}" || true
+fi
