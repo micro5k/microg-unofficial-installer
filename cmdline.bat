534b3bf567eb45094ae06c653be91afe9e960e8b
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2022-12-19 15:22:35 +0100

    Minor change

--- cmdline.bat
+++ cmdline.bat
@@ -9,6 +9,7 @@ REM Fix the working directory when using "Run as administrator"
 IF "%CD%" == "%windir%\system32" CD /D "%~dp0"
 
 SET "LANG=C.UTF-8"
+CHCP 65001 1> nul || ECHO "Changing the codepage failed"
 
 SET "PATH=.;%PATH%"
 IF EXIST "%~dp0tools\win" SET "PATH=%~dp0tools\win;%PATH%"
@@ -19,7 +20,6 @@ SET "SCRIPT_DIR=%~dp0"
 SET "BASH_VERSION=5.0.17(1)-release"
 SET "BASH_VERSINFO=5"
 
-CHCP 65001 1> nul || ECHO "Changing the codepage failed"
 "%~dp0tools\win\busybox.exe" ash -s -c ". '%~dp0scripts\common.sh' || exit 1; change_title 'Command-line'; unset JAVA_HOME; alias dir=ls; alias 'cd..'='cd ..'; alias 'cd.'='cd .'; alias cls=reset" "%~f0" %*
 
 ENDLOCAL 2> nul
