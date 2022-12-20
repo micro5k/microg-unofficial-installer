dc0735eecb02fb49db1457eefee53598a32c8c3a
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2022-12-20 16:41:04 +0100

    Minor improvements

--- zip-content/zip-install.sh
+++ zip-content/zip-install.sh
@@ -3,15 +3,20 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # SPDX-FileType: SOURCE
 
+ui_show_error()
+{
+  printf 1>&2 '\033[1;31mERROR: %s\033[0m\n' "${1:?}"
+}
+
 umask 022 || exit 1
 
 if test "$(whoami || true)" != 'root'; then
-  printf 'ERROR: %s\n' 'You must execute it as root'
+  ui_show_error 'You must execute this as root'
   exit 2
 fi
 
 if test -z "${1:-}" || test ! -e "${1:?}"; then
-  printf 'ERROR: %s\n' 'You must specify the ZIP file to install'
+  ui_show_error 'You must specify the ZIP file to install'
   exit 2
 fi
 ZIPFILE="$(realpath -- "${1:?}")" || ZIPFILE="$(readlink -f -- "${1:?}")" || exit 2
@@ -37,18 +42,18 @@ if test -n "${TMPDIR:-}" && test -w "${TMPDIR:?}"; then
 elif test -w '/tmp'; then
   TMPDIR='/tmp'
 elif test -e '/dev'; then
-  mkdir -p -- '/dev/tmp' || { printf 'ERROR: %s\n' 'Failed to create a temp folder'; exit 3; }
+  mkdir -p -- '/dev/tmp' || { ui_show_error 'Failed to create a temp folder'; exit 3; }
   TMPDIR='/dev/tmp'
 fi
 
 if test -z "${TMPDIR:-}" || test ! -w "${TMPDIR:?}"; then
-  printf 'ERROR: %s\n' 'Unable to create a temp folder'
+  ui_show_error 'Unable to create a temp folder'
   exit 4
 fi
 export TMPDIR
 
 SCRIPT_NAME="${TMPDIR:?}/update-binary.sh" || exit 5
-unzip -p -qq "${ZIPFILE:?}" 'META-INF/com/google/android/update-binary' 1> "${SCRIPT_NAME:?}" || { printf 'ERROR: %s\n' 'Failed to extract update-binary'; exit 6; }
+unzip -p -qq "${ZIPFILE:?}" 'META-INF/com/google/android/update-binary' 1> "${SCRIPT_NAME:?}" || { ui_show_error 'Failed to extract update-binary'; exit 6; }
 
 STATUS=0
 sh -- "${SCRIPT_NAME:?}" 3 1 "${ZIPFILE:?}" || STATUS="${?}"
@@ -59,6 +64,6 @@ _clean_at_exit
 trap - 0 2 3 6 15 || true
 
 if test "${STATUS:-1}" != '0'; then
-  printf 'ERROR: %s\n' 'ZIP installation failed'
+  ui_show_error 'ZIP installation failed'
   exit "${STATUS:-1}"
 fi
