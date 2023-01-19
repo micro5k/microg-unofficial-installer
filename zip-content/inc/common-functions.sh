88a3dbc11e36a6eb622a62f8b4ca869850ddc549
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2023-01-19 14:27:36 +0100

    Minor change

--- zip-content/inc/common-functions.sh
+++ zip-content/inc/common-functions.sh
@@ -240,10 +240,11 @@ _advanced_find_and_mount_system()
 
 _find_and_mount_system()
 {
+  SYS_MOUNTPOINT_LIST='' # This is a list of paths separated by spaces, the path itself cannot contains spaces
+
   if test "${TEST_INSTALL:-false}" != 'false' && test -n "${ANDROID_ROOT:-}" && test -e "${ANDROID_ROOT:?}"; then
     SYS_MOUNTPOINT_LIST="${ANDROID_ROOT:?}"
   else
-    SYS_MOUNTPOINT_LIST=''
     if test -e '/mnt/system'; then SYS_MOUNTPOINT_LIST="${SYS_MOUNTPOINT_LIST?} /mnt/system"; fi
     if test -n "${ANDROID_ROOT:-}" && test -e "${ANDROID_ROOT:?}"; then SYS_MOUNTPOINT_LIST="${SYS_MOUNTPOINT_LIST?} ${ANDROID_ROOT:?}"; fi
     if test "${ANDROID_ROOT:-}" != '/system_root' && test -e '/system_root'; then SYS_MOUNTPOINT_LIST="${SYS_MOUNTPOINT_LIST?} /system_root"; fi
