1cdbf36298569db27d063e708bc235dc67903d96
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2023-01-22 23:06:07 +0100

    Minor changes

--- zip-content/scripts/install.sh
+++ zip-content/scripts/install.sh
@@ -137,7 +137,7 @@ ui_msg "Dynamic partitions: ${DYNAMIC_PARTITIONS:?}"
 ui_msg "Current slot: ${SLOT:-no slot}"
 ui_msg "Recov. fake system: ${RECOVERY_FAKE_SYSTEM:?}"
 ui_msg_empty_line
-ui_msg "System mount point: ${MOUNT_POINT:?}"
+ui_msg "System mount point: ${SYS_MOUNTPOINT:?}"
 ui_msg "System path: ${SYS_PATH:?}"
 ui_msg "Priv-app path: ${PRIVAPP_PATH:?}"
 ui_msg_empty_line
