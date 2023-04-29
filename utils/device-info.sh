17a9c33ec8a4f5d72877e2c629018d9cdec8cb28
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2023-04-29 18:09:57 +0200

    Minor changes

--- utils/device-info.sh
+++ utils/device-info.sh
@@ -11,11 +11,10 @@
 # shellcheck disable=SC3043 # In POSIX sh, local is undefined
 
 set -u
-# shellcheck disable=SC3040,SC2015
+# shellcheck disable=SC3040,SC3041,SC2015
 {
   # Unsupported set options may cause the shell to exit (even without set -e), so first try them in a subshell to avoid this issue
   (set -o posix 2> /dev/null) && set -o posix || true
-  # shellcheck disable=SC3041
   (set +H 2> /dev/null) && set +H || true
   (set -o pipefail) && set -o pipefail || true
 }
@@ -188,8 +187,8 @@ contains()
 trim_space_on_sides()
 {
   local _var
-  IFS='' read -r _var || return 1
-  test "${#_var}" -ne 0 || return 1
+  _var="$(cat -u)" || return 1
+  test "${#_var}" -gt 0 || return 1
 
   _var="${_var# }"
   printf '%s' "${_var% }"
@@ -312,9 +311,7 @@ get_advertising_id()
 
 get_phone_info()
 {
-  adb shell "service call iphonesubinfo ${*}" | cut -d "'" -f '2' -s | {
-    LC_ALL=C tr -d -s -- '.[:cntrl:]' '[:space:]' && printf '\n'
-  } | trim_space_on_sides
+  adb shell "service call iphonesubinfo ${*}" | cut -d "'" -f '2' -s | LC_ALL=C tr -d -s '.[:cntrl:]' '[:space:]' | trim_space_on_sides
 }
 
 display_info()
