60b97cfd3d3b0f03a739a58b5f3da71c5d857ab7
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2023-05-01 02:19:16 +0200

    Minor change

--- recovery-simulator/recovery.sh
+++ recovery-simulator/recovery.sh
@@ -189,6 +189,8 @@ link_folder "${BASE_SIMULATION_PATH:?}/sdcard" "${_android_ext_stor:?}"
   echo 'ro.product.cpu.abilist=x86_64,x86,arm64-v8a,armeabi-v7a,armeabi'
   echo 'ro.product.cpu.abilist32=x86,armeabi-v7a,armeabi'
   echo 'ro.product.cpu.abilist64=x86_64,arm64-v8a'
+  echo 'ro.product.device=emu64x'
+  echo 'ro.build.characteristics=phone,emulator'
 } 1> "${_android_sys:?}/build.prop"
 
 touch "${BASE_SIMULATION_PATH:?}/AndroidManifest.xml"
