a95bc8e7cb29a4bcf783cdd64d7fc35e81c54367
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2023-08-26 15:07:43 +0200

    Minor improvements

--- utils/device-info.sh
+++ utils/device-info.sh
@@ -381,29 +381,29 @@ validate_and_display_info()
 
 get_imei_via_MMI_code()
 {
-  adb 2> /dev/null shell "
-      test -e '/proc/self/fd/1' || exit 1
-      am 1> /dev/null 2>&1 start -a 'android.intent.action.DIAL' || true
+  adb 2> /dev/null shell '
+      test -e "/proc/self/fd/1" || exit 1
+      am 1> /dev/null 2>&1 start -a "com.android.phone.action.TOUCH_DIALER"
 
-      # If we're not in the dialpad, give up
-      uiautomator 2> /dev/null dump --compressed '/proc/self/fd/1' | grep -q -F -e 'com.android.dialer:id/dialpad_key_number' || exit 1
+      # If we are NOT in the dialpad, give up
+      uiautomator 2> /dev/null dump --compressed "/proc/self/fd/1" | grep -q -F -e "com.android.dialer:id/dialpad_key_number" -e "com.android.contacts:id/dialpad_key_letters" || exit 2
 
       input keyevent KEYCODE_STAR &&
       input keyevent KEYCODE_POUND &&
-      input text '06' &&
+      input text "06" &&
       input keyevent KEYCODE_POUND
 
-      uiautomator 2> /dev/null dump --compressed '/proc/self/fd/1'
+      uiautomator 2> /dev/null dump --compressed "/proc/self/fd/1" || exit 3
 
-      input keyevent KEYCODE_ENTER &&
-      input keyevent KEYCODE_ENTER &&
+      input keyevent KEYCODE_ENTER
       input keyevent KEYCODE_HOME
-    " |
+    ' |
     sed 's/>/>\n/g' |
     grep -F -m 1 -A 1 -e 'IMEI' |
     tail -n 1 |
-    grep -o -m 1 -e 'text="[0-9]*"' |
-    cut -d '"' -f '2' -s
+    grep -o -m 1 -e 'text="[0-9 /]*"' |
+    cut -d '"' -f '2' -s |
+    LC_ALL=C tr -d ' '
 }
 
 get_imei()
