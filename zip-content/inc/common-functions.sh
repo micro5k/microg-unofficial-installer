fa07e997392787d22521f5991bb6f38b6412538f
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2023-09-11 23:51:04 +0200

    Detect fake writability

--- zip-content/inc/common-functions.sh
+++ zip-content/inc/common-functions.sh
@@ -794,6 +794,12 @@ clean_previous_installations()
 {
   local _initial_free_space
 
+  if touch 2> /dev/null -- "${SYS_PATH:?}/etc/write-test-file.dat" && test -e "${SYS_PATH:?}/etc/write-test-file.dat" && rm -f -- "${SYS_PATH:?}/etc/write-test-file.dat"; then
+    : # Really writable
+  else
+    ui_error "Something is wrong because '${SYS_PATH?}' is NOT really writable!!!"
+  fi
+
   if test "${FIRST_INSTALLATION:?}" = 'true'; then
     _initial_free_space='-1'
   else
