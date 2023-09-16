1936b9c221cd808f0b9acf86d825aaedbb7e95d6
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2023-09-16 18:07:17 +0200

    Minor change

--- conf-2.sh
+++ conf-2.sh
@@ -6,8 +6,8 @@
 files_to_download()
 {
   cat << 'EOF'
-PlayStoreLegacy|priv-app|9|22|Phonesky|com.android.vending|d78b377db43a2bc0570f37b2dd0efa4ec0b95746|www.apkmirror.com/apk/google-inc/google-play-store/google-play-store-5-1-11-3-release/google-play-store-5-1-11-android-apk-download/|store5.gofile.io/download/53be4c41-0d3d-42d4-819c-a1e103781bfc/PlayStoreLegacy.apk
 PlayStore|priv-app|23||Phonesky|com.android.vending|2dc60fc611330609fb11db7193356772aa91d20e|www.apkmirror.com/apk/google-inc/google-play-store/google-play-store-22-6-29-release/google-play-store-22-6-29-16-0-pr-341141187-android-apk-download/|
+PlayStoreLegacy|priv-app|9|22|Phonesky|com.android.vending|d78b377db43a2bc0570f37b2dd0efa4ec0b95746|www.apkmirror.com/apk/google-inc/google-play-store/google-play-store-5-1-11-3-release/google-play-store-5-1-11-android-apk-download/|store5.gofile.io/download/53be4c41-0d3d-42d4-819c-a1e103781bfc/PlayStoreLegacy.apk
 AndroidAuto|priv-app|23||AndroidAuto|com.google.android.projection.gearhead|1ebc63051e5728030eac615210cbdaa25afdc0cb|www.apkmirror.com/apk/google-inc/android-auto/android-auto-1-2-520120-stub-release/android-auto-1-2-520120-stub-android-apk-download/|store5.gofile.io/download/c2f9ef99-f036-4c67-b73e-2df0d3055545/AndroidAuto.apk
 EOF
 }
