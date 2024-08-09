002a9819572030bb1cd41ce48037ae20557eed48
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2024-08-09 16:46:02 +0200

    Minor change

---
+++ tools/win/start.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: (c) 2024 ale5000
+# SPDX-License-Identifier: GPL-3.0-or-later
+# shellcheck enable=all
+
+MSYS_NO_PATHCONV=1 "${COMSPEC:?}" /c start "${@}"
