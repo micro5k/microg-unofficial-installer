addc028284b70a4e64e69c33deba2e22535efef9
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2024-09-25 14:51:22 +0200

    Fix for broken shells

--- tools/bits-info.sh
+++ tools/bits-info.sh
@@ -5,12 +5,7 @@
 # shellcheck enable=all
 # shellcheck disable=SC3043 # In POSIX sh, local is undefined
 
-set -u
-# shellcheck disable=SC3040,SC2015 # Ignore: In POSIX sh, set option xxx is undefined / C may run when A is true
-{
-  # Unsupported set options may cause the shell to exit (even without set -e), so first try them in a subshell to avoid this issue
-  (set 2> /dev/null -o pipefail) && set -o pipefail || true
-}
+set -u || true
 setopt 2> /dev/null SH_WORD_SPLIT || true
 
 convert_max_signed_int_to_bit()
@@ -83,10 +78,10 @@ get_shell_info()
 
   if _shell_exe="$(readlink 2> /dev/null "/proc/${$}/exe")" && test -n "${_shell_exe?}"; then
     :
-  elif _shell_exe="${SHELL-}" && test -n "${_shell_exe?}"; then
+  elif _shell_exe="${BASH:-${SHELL-}}" && test -n "${_shell_exe?}"; then
     :
   else
-    printf '%s\n' 'not-found'
+    printf '%s\n' 'not-found unknown'
     return 1
   fi
 
@@ -119,7 +114,7 @@ get_shell_info()
     fi
 
     case "${_shell_version?}" in
-      '' | *'invalid option'* | *'unrecognized option'* | *'unknown option'* | *[Ii]'llegal option'* | *'not an option'*)
+      '' | *'invalid option'* | *'unrecognized option'* | *'unknown option'* | *[Ii]'llegal option'* | *'not an option'* | *'bad option'* | *'command not found'*)
         if test "${_shell_name?}" = 'dash' && command 1> /dev/null -v 'dpkg' && _shell_version="$(dpkg -s 'dash' | grep -m 1 -F -e 'Version:' | cut -d ':' -f '2-' -s)"; then
           : # For dash
         elif test "${_shell_name?}" = 'dash' && test -n "${DASH_VERSION-}" && _shell_version="${DASH_VERSION:?}"; then
@@ -186,7 +181,8 @@ get_os_info()
 {
   local _os_name _os_version 2> /dev/null
 
-  _os_name="$(uname 2> /dev/null -o || uname 2> /dev/null || true)"
+  # Bugged versions of uname may return errors on STDOUT when used with unsupported options
+  _os_name="$(uname 2> /dev/null -o)" || _os_name="$(uname 2> /dev/null)" || _os_name=''
   _os_version=''
 
   case "${_os_name?}" in
@@ -197,6 +193,9 @@ get_os_info()
       _os_name='MS/Windows'
       _os_version="$(uname | cut -d '-' -f '2-' -s | tr -- '-' '.' || true)"
       ;;
+    'Windows_NT') # Bugged versions of uname: it doesn't support uname -o and it is unable to retrieve the version of Windows
+      _os_name='MS/Windows'
+      ;;
     'GNU/Linux')
       if _os_version="$(getprop 2> /dev/null 'ro.build.version.release')" && test -n "${_os_version?}"; then
         _os_name='Android'
@@ -205,7 +204,7 @@ get_os_info()
       fi
       ;;
     *)
-      _os_version="$(uname 2> /dev/null -r || true)"
+      _os_version="$(uname 2> /dev/null -r)" || _os_version=''
       ;;
   esac
 
@@ -273,7 +272,7 @@ main()
   _limits_date='32767 2147480047 2147483647 32535215999 32535244799 67767976233529199 67767976233532799 67768036191673199 67768036191676799 9223372036854775807'
   _limits_u='65535 2147483647 2147483648 4294967295 18446744073709551615'
 
-  shell_info="$(get_shell_info)" || shell_info='unknown unknown'
+  shell_info="$(get_shell_info || true)"
   shell_name="$(printf '%s\n' "${shell_info:?}" | cut -d ' ' -f '1')" || shell_name='unknown'
 
   case "$(uname -m || true)" in
@@ -289,7 +288,7 @@ main()
       x86) os_bit='32-bit' ;;
       *) os_bit='unknown' ;;
     esac
-  elif command 1> /dev/null -v 'getconf' && os_bit="$(getconf 'LONG_BIT')" && test -n "${os_bit?}"; then
+  elif command 1> /dev/null 2>&1 -v 'getconf' && os_bit="$(getconf 'LONG_BIT')" && test -n "${os_bit?}"; then
     os_bit="${os_bit:?}-bit"
   elif test -e '/system/build.prop'; then
     # On Android
@@ -308,13 +307,13 @@ main()
     else
       cpu_bit='32-bit'
     fi
-  elif command 1> /dev/null -v 'wmic.exe' && cpu_bit="$(MSYS_NO_PATHCONV=1 wmic.exe cpu get DataWidth /VALUE | cut -d '=' -f '2-' -s | tr -d '\r')"; then
+  elif command 1> /dev/null 2>&1 -v 'wmic.exe' && cpu_bit="$(MSYS_NO_PATHCONV=1 wmic.exe cpu get DataWidth /VALUE | cut -d '=' -f '2-' -s | tr -d '\r')"; then
     # On Windows / ReactOS (if WMIC is present)
     case "${cpu_bit?}" in
       '64' | '32') cpu_bit="${cpu_bit:?}-bit" ;;
       *) cpu_bit='unknown' ;;
     esac
-  elif command 1> /dev/null -v 'powershell.exe' && cpu_bit="$(powershell.exe -c 'gwmi Win32_Processor | select -ExpandProperty DataWidth')"; then
+  elif command 1> /dev/null 2>&1 -v 'powershell.exe' && cpu_bit="$(powershell.exe -c 'gwmi Win32_Processor | select -ExpandProperty DataWidth')"; then
     # On Windows (if PowerShell is installed - it is slow)
     case "${cpu_bit?}" in
       '64' | '32') cpu_bit="${cpu_bit:?}-bit" ;;
