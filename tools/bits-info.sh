a69ed4ca7cac5bb010c9c683b046691122f90579
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2024-09-26 22:01:25 +0200

    Minor change

--- tools/bits-info.sh
+++ tools/bits-info.sh
@@ -307,7 +307,7 @@ main()
   shell_info="$(get_shell_info || true)"
   shell_name="$(printf '%s\n' "${shell_info:?}" | cut -d ' ' -f '1' || true)"
 
-  if tmp_var="$(hexdump 2> /dev/null -v -n 5 -e '/1 "%02x"' 0< "/proc/${$}/exe")" && test -n "${tmp_var?}" && printf '%s\n' "${tmp_var:?}" | grep -m 1 -q -e '^7f454c46'; then
+  if test -n "${shell_exe?}" && tmp_var="$(hexdump 2> /dev/null -v -n 5 -e '/1 "%02x"' -- "${shell_exe:?}")" && test -n "${tmp_var?}" && printf '%s\n' "${tmp_var:?}" | grep -m 1 -q -e '^7f454c46'; then
     # On Linux / Android
     # ELF header => 0x7F + ELF (0x45 0x4C 0x46) + 0x01 for 32-bit or 0x02 for 64-bit
     case "${tmp_var?}" in
