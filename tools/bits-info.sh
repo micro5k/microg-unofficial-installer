adf92016e9d8802ca1407f3d264220bc0f411305
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2024-09-27 14:29:13 +0200

    Minor changes

--- tools/bits-info.sh
+++ tools/bits-info.sh
@@ -133,6 +133,25 @@ check_bitness_of_pe_file()
   return 3
 }
 
+get_shell_exe()
+{
+  local _gse_shell_exe _gse_tmp_var 2> /dev/null
+
+  if _gse_shell_exe="$(readlink 2> /dev/null "/proc/${$}/exe")" && test -n "${_gse_shell_exe?}"; then
+    # On Linux / Android / Windows (on Windows only some shells support it)
+    :
+  elif _gse_tmp_var="$(ps 2> /dev/null -p "${$}" -o 'comm=')" && test -n "${_gse_tmp_var?}" && _gse_tmp_var="$(command 2> /dev/null -v "${_gse_tmp_var:?}")"; then
+    # On Linux / macOS
+    _gse_shell_exe="$(readlink 2> /dev/null -f "${_gse_tmp_var:?}" || realpath 2> /dev/null "${_gse_tmp_var:?}")" || _gse_shell_exe="${_gse_tmp_var:?}"
+  elif _gse_tmp_var="${BASH:-${SHELL-}}" && test -n "${_gse_tmp_var?}"; then
+    _gse_shell_exe="$(readlink 2> /dev/null -f "${_gse_tmp_var:?}" || realpath 2> /dev/null "${_gse_tmp_var:?}")" || _gse_shell_exe="${_gse_tmp_var:?}"
+  else
+    return 1
+  fi
+
+  printf '%s\n' "${_gse_shell_exe:?}"
+}
+
 get_shell_info()
 {
   local _shell_use_ver_opt _shell_exe _shell_name _shell_version _tmp_var 2> /dev/null
@@ -325,15 +344,7 @@ main()
   _limits_date='32767 2147480047 2147483647 32535215999 32535244799 67767976233529199 67767976233532799 67768036191673199 67768036191676799 9223372036854775807'
   _limits_u='65535 2147483647 2147483648 4294967295 18446744073709551615'
 
-  if shell_exe="$(readlink 2> /dev/null "/proc/${$}/exe")" && test -n "${shell_exe?}"; then # On Linux / Android / Windows
-    :
-  elif tmp_var="$(ps 2> /dev/null -p "${$}" -o 'comm=')" && test -n "${tmp_var?}" && tmp_var="$(command 2> /dev/null -v "${tmp_var:?}")"; then # On Linux / macOS
-    shell_exe="$(readlink 2> /dev/null -f "${tmp_var:?}" || realpath 2> /dev/null "${tmp_var:?}")" || shell_exe="${tmp_var:?}"
-  elif tmp_var="${BASH:-${SHELL-}}" && test -n "${tmp_var?}"; then
-    shell_exe="$(readlink 2> /dev/null -f "${tmp_var:?}" || realpath 2> /dev/null "${tmp_var:?}")" || shell_exe="${tmp_var:?}"
-  else
-    shell_exe=''
-  fi
+  shell_exe="$(get_shell_exe || :)"
 
   shell_info="$(get_shell_info || true)"
   shell_name="$(printf '%s\n' "${shell_info:?}" | cut -d ' ' -f '1' || true)"
