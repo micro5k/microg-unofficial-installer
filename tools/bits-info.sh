1583d8560862adb409efd8e6c5bc7c346a71b4a1
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2024-09-29 18:28:18 +0200

    Minor changes

--- tools/bits-info.sh
+++ tools/bits-info.sh
@@ -180,25 +180,46 @@ check_bitness_of_file()
     # MZ - Executable binaries for Windows / DOS (.exe) - Start with: MZ (0x4D 0x5A)
 
     _cbf_needs_bytes_swap='true'
+    if _cbf_pos="$(dump_hex "${1:?}" '0x3C' '4')" && test "${_cbf_pos?}" != '00000000' && _cbf_pos="$(switch_endianness_4 "${_cbf_pos?}")" && _header="$(dump_hex "${1:?}" "0x${_cbf_pos:?}" '6')"; then
+      :
+    else _header=''; fi
 
-    if _cbf_pos="$(dump_hex "${1:?}" '0x3C' '4')" && _cbf_pos="$(switch_endianness_4 "${_cbf_pos?}")" && _header="$(dump_hex "${1:?}" "0x${_cbf_pos:?}" '6')" &&
-      printf '%s\n' "${_header?}" | grep -m 1 -q -e '^50450000'; then
-      # PE header => PE (0x50 0x45) + 0x00 + 0x00 + Machine field
-      # More info: https://learn.microsoft.com/en-us/windows/win32/debug/pe-format
+    if test -n "${_header?}"; then
+      if printf '%s\n' "${_header:?}" | grep -m 1 -q -e '^50450000'; then
+        # PE header => PE (0x50 0x45) + 0x00 + 0x00 + Machine field
+        # More info: https://learn.microsoft.com/en-us/windows/win32/debug/pe-format
 
-      case "${_header?}" in
-        *'6486') printf '%s\n' '64-bit PE (x86-64)' ;; # x86-64 (0x64 0x86) - also called AMD64
-        *'64aa') printf '%s\n' '64-bit PE (ARM64)' ;;  # ARM64  (0x64 0xAA)
-        *'0002') printf '%s\n' '64-bit PE (IA-64)' ;;  # IA-64  (0x00 0x02)
-        *'4c01') printf '%s\n' '32-bit PE (x86)' ;;    # x86    (0x4C 0x01)
-        *'c001') printf '%s\n' '64-bit PE (ARM)' ;;    # ARM    (0xC0 0x01)
-        *'0000') printf '%s\n' '16-bit PE' ;;          # Any    (0x00 0x00)
-        *)
-          printf '%s\n' 'unknown-pe-file'
-          return 4
-          ;;
-      esac
-      return 0
+        case "${_header:?}" in
+          *'6486') printf '%s\n' '64-bit PE (x86-64)' ;; # x86-64 (0x64 0x86) - also called AMD64
+          *'64aa') printf '%s\n' '64-bit PE (ARM64)' ;;  # ARM64  (0x64 0xAA)
+          *'0002') printf '%s\n' '64-bit PE (IA-64)' ;;  # IA-64  (0x00 0x02)
+          *'4c01') printf '%s\n' '32-bit PE (x86)' ;;    # x86    (0x4C 0x01)
+          *'c001') printf '%s\n' '64-bit PE (ARM)' ;;    # ARM    (0xC0 0x01)
+          *'0000') printf '%s\n' '16-bit PE' ;;          # Any    (0x00 0x00)
+          *)
+            printf '%s\n' 'unknown-pe-file'
+            return 4
+            ;;
+        esac
+        return 0
+      else
+        _header="$(extract_bytes "${_header:?}" '0' '2' || :)"
+        case "${_header?}" in
+          '4c45')
+            # LE (Linear Executable) header => LE (0x4C 0x45)
+            printf '%s\n' '16/32-bit LE'
+            return 0
+            ;;
+          '4c58')
+            # LX (Linear Executable) header => LE (0x4C 0x58)
+            printf '%s\n' '32-bit LX'
+            return 0
+            ;;
+          *) ;;
+        esac
+      fi
+
+      #printf '\n' && hexdump -v -C -s "0x${_cbf_pos:?}" -n '6' -- "${1:?}" # Debug
     fi
 
     if _cbf_tmp_var="$(dump_hex "${1:?}" '0x18' '2')" && _cbf_tmp_var="$(hex_bytes_to_int "${_cbf_tmp_var?}" '2' "${_cbf_needs_bytes_swap:?}")" && test "${_cbf_tmp_var:?}" -lt 64; then
@@ -297,14 +318,16 @@ check_bitness_of_file()
     fi
   fi
 
-  _cbf_size="$(stat -c '%s' -- "${1:?}")"
+  _cbf_size="$(stat -c '%s' -- "${1:?}")" || {
+    printf '%s\n' 'failed'
+    return 1
+  }
 
-  if _cbf_tmp_var="$(extract_bytes "${_cbf_first_8_bytes?}" '0' '1')" &&
+  if test "${_cbf_size:?}" -le 65280 && test "${_cbf_size:?}" -ge 2 && _cbf_tmp_var="$(extract_bytes "${_cbf_first_8_bytes?}" '0' '1')" &&
     {
       test "${_cbf_tmp_var?}" = 'e9' || test "${_cbf_tmp_var?}" = 'eb' ||
         test "$(extract_bytes "${_cbf_first_8_bytes?}" '0' '2' || :)" = '81fc'
-    } &&
-    test "${_cbf_size:?}" -le 65280; then
+    }; then
     # COM - Executable binaries for DOS (.com)
 
     # To detect COM programs we can check if the first byte of the file could be a valid jump or call opcode (most common: 0xE9 or 0xEB).
