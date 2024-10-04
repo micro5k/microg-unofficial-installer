c055b5b7027e94fac7f10ae8d356c14f7010577a
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2024-10-05 01:15:05 +0200

    Minor changes

--- cmdline.sh
+++ cmdline.sh
@@ -12,7 +12,7 @@ if test "${A5K_FUNCTIONS_INCLUDED:-false}" = 'false'; then
     # Execute only if the first initialization has not already been done
     if test -z "${MAIN_DIR-}" || test -z "${USER_HOME-}"; then
 
-      # Prevent picturesque bugs on Bash under Windows
+      # Avoid picturesque bugs on Bash under Windows
       if test -e '/usr/bin' && test "$(/usr/bin/uname 2> /dev/null -o || :)" = 'Msys'; then PATH="/usr/bin:${PATH:-/usr/bin}"; fi
 
       if test -z "${MAIN_DIR-}"; then
@@ -42,6 +42,27 @@ if test "${A5K_FUNCTIONS_INCLUDED:-false}" = 'false'; then
     if test "${PLATFORM-}" = 'win' && test "${IS_BUSYBOX-}" = 'true'; then
       exec ash -s -c ". '${_main_dir:?}/includes/common.sh' || exit \${?}" 'ash' "${@}"
     else
+      get_shell_exe()
+      {
+        local _gse_shell_exe _gse_tmp_var
+
+        if _gse_shell_exe="$(readlink 2> /dev/null "/proc/${$}/exe")" && test -n "${_gse_shell_exe?}"; then
+          # On Linux / Android / Windows (on Windows only some shells support it)
+          :
+        elif _gse_tmp_var="$(ps 2> /dev/null -p "${$}" -o 'comm=')" && test -n "${_gse_tmp_var?}" && _gse_tmp_var="$(command 2> /dev/null -v "${_gse_tmp_var:?}")"; then
+          # On Linux / macOS
+          _gse_shell_exe="$(readlink 2> /dev/null -f "${_gse_tmp_var:?}" || realpath 2> /dev/null "${_gse_tmp_var:?}")" || _gse_shell_exe="${_gse_tmp_var:?}"
+        elif _gse_tmp_var="${BASH:-${SHELL-}}" && test -n "${_gse_tmp_var?}"; then
+          if test ! -e "${_gse_tmp_var:?}" && test -e "${_gse_tmp_var:?}.exe"; then _gse_tmp_var="${_gse_tmp_var:?}.exe"; fi # Special fix for broken versions of Bash under Windows
+          _gse_shell_exe="$(readlink 2> /dev/null -f "${_gse_tmp_var:?}" || realpath 2> /dev/null "${_gse_tmp_var:?}")" || _gse_shell_exe="${_gse_tmp_var:?}"
+          _gse_shell_exe="$(command 2> /dev/null -v "${_gse_shell_exe:?}")" || return 1
+        else
+          return 1
+        fi
+
+        printf '%s\n' "${_gse_shell_exe:?}"
+      }
+
       if test "${#}" -gt 0; then
         _newline='
 '
@@ -57,11 +78,7 @@ if test "${A5K_FUNCTIONS_INCLUDED:-false}" = 'false'; then
         esac
       fi
 
-      if __SHELL_EXE="$(readlink 2> /dev/null "/proc/${$}/exe")" && test -n "${__SHELL_EXE?}"; then
-        :
-      else
-        __SHELL_EXE="${SHELL:-bash}"
-      fi
+      __SHELL_EXE="$(get_shell_exe)" || __SHELL_EXE='bash'
       export __SHELL_EXE
 
       exec "${__SHELL_EXE:?}" --init-file "${_main_dir:?}/includes/common.sh"
