0e6e24d50268b3b027c65ae63f5d99d886e0be18
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2024-10-10 19:17:53 +0200

    Minor change

--- cmdline.sh
+++ cmdline.sh
@@ -45,21 +45,22 @@ if test "${A5K_FUNCTIONS_INCLUDED:-false}" = 'false'; then
 
       if _gse_shell_exe="$(readlink 2> /dev/null "/proc/${$}/exe")" && test -n "${_gse_shell_exe}"; then
         # On Linux / Android / Windows (on Windows only some shells support it)
-        :
+        printf '%s\n' "${_gse_shell_exe}"
+        return 0
       elif _gse_tmp_var="$(ps 2> /dev/null -p "${$}" -o 'comm=')" && test -n "${_gse_tmp_var}" && _gse_tmp_var="$(command 2> /dev/null -v "${_gse_tmp_var}")"; then
         # On Linux / macOS
         # shellcheck disable=SC2230 # Ignore: 'which' is non-standard
         test "${_gse_tmp_var}" != 'osh' || _gse_tmp_var="$(which 2> /dev/null "${_gse_tmp_var}")" || return 3 # We may not get the full path with "command -v" on osh
-        _gse_shell_exe="$(readlink 2> /dev/null -f "${_gse_tmp_var}" || realpath 2> /dev/null "${_gse_tmp_var}")" || _gse_shell_exe="${_gse_tmp_var}"
       elif _gse_tmp_var="${BASH:-${SHELL-}}" && test -n "${_gse_tmp_var}"; then
+        if test "${_gse_tmp_var}" = '/bin/sh' && test "$(uname 2> /dev/null || :)" = 'Windows_NT'; then _gse_tmp_var="$(command 2> /dev/null -v 'busybox')" || return 2; fi
         if test ! -e "${_gse_tmp_var}" && test -e "${_gse_tmp_var}.exe"; then _gse_tmp_var="${_gse_tmp_var}.exe"; fi # Special fix for broken versions of Bash under Windows
-        _gse_shell_exe="$(readlink 2> /dev/null -f "${_gse_tmp_var}" || realpath 2> /dev/null "${_gse_tmp_var}")" || _gse_shell_exe="${_gse_tmp_var}"
-        _gse_shell_exe="$(command 2> /dev/null -v "${_gse_shell_exe}")" || return 2
       else
         return 1
       fi
 
+      _gse_shell_exe="$(readlink 2> /dev/null -f "${_gse_tmp_var}" || realpath 2> /dev/null "${_gse_tmp_var}")" || _gse_shell_exe="${_gse_tmp_var}"
       printf '%s\n' "${_gse_shell_exe}"
+      return 0
     }
 
     __SHELL_EXE="$(get_shell_exe)" || __SHELL_EXE='bash'
