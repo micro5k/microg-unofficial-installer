08dcf378c0084539c4a91ed61df868687915cc4a
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2024-11-18 20:37:14 +0100

    Minor improvements

--- zip-content/zip-install.sh
+++ zip-content/zip-install.sh
@@ -2,7 +2,7 @@
 # SPDX-FileCopyrightText: (c) 2022 ale5000
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-readonly ZIPINSTALL_VERSION='1.2.1'
+readonly ZIPINSTALL_VERSION='1.2.2'
 
 umask 022 || :
 PATH="${PATH:-/system/bin}:."
@@ -69,33 +69,50 @@ command 1> /dev/null -v head || {
 
 ### FUNCTIONS AND CODE ###
 
-ui_show_error()
+ui_info_msg()
 {
-  printf 1>&2 '\033[1;31m%s\033[0m\n' "ERROR: ${1:-}"
+  if test -n "${NO_COLOR-}"; then
+    printf '%s\n' "${1}"
+  elif test "${CI:-false}" = 'false'; then
+    printf '\033[1;32m\r%s\n\033[0m\r    \r' "${1}"
+  else
+    printf '\033[1;32m%s\033[0m\n' "${1}"
+  fi
+}
+
+ui_error_msg()
+{
+  if test -n "${NO_COLOR-}"; then
+    printf 1>&2 '%s\n' "ERROR: ${1}"
+  elif test "${CI:-false}" = 'false'; then
+    printf 1>&2 '\033[1;31m\r%s\n\033[0m\r    \r' "ERROR: ${1}"
+  else
+    printf 1>&2 '\033[1;31m%s\033[0m\n' "ERROR: ${1}"
+  fi
 }
 
 if test -n "${*}"; then
   for _param in "${@}"; do
     shift || {
-      ui_show_error 'shift failed'
+      ui_error_msg 'shift failed'
       exit 6
     }
     # Skip empty parameters or parameters that may get passed due to buggy su implementation
     if test -z "${_param:-}" || test "${_param:?}" = '--' || test "${_param:?}" = '[su]zip-install.sh'; then continue; fi
 
     test -e "${_param:?}" || {
-      ui_show_error "ZIP file doesn't exist => '${_param:-}'"
+      ui_error_msg "ZIP file doesn't exist => '${_param:-}'"
       exit 7
     }
 
     _param_copy="${_param:?}"
     _param="$(readlink -f "${_param_copy:?}")" || _param="$(realpath "${_param_copy:?}")" || {
-      ui_show_error "Canonicalization failed => '${_param_copy:-}'"
+      ui_error_msg "Canonicalization failed => '${_param_copy:-}'"
       exit 8
     }
 
     set -- "${@}" "${_param:?}" || {
-      ui_show_error 'set failed'
+      ui_error_msg 'set failed'
       exit 6
     }
   done
@@ -103,7 +120,7 @@ if test -n "${*}"; then
 fi
 
 if test -z "${*}"; then
-  ui_show_error 'You must specify the ZIP file to install'
+  ui_error_msg 'You must specify the ZIP file to install'
   exit 5
 fi
 
@@ -114,19 +131,19 @@ if test "$(whoami || :)" != 'root'; then
     # First verify that "su" is working (user 0 is root)
     su 0 sh -c 'command' '[su]verification' || {
       _status="${?}" # Usually it return 1 or 255 when root is present but disabled
-      ui_show_error 'Auto-rooting failed, you must execute this as root!!!'
+      ui_error_msg 'Auto-rooting failed, you must execute this as root!!!'
       exit "${_status:-2}"
     }
 
     ZIP_INSTALL_SCRIPT="$(readlink -f "${0:?}")" || ZIP_INSTALL_SCRIPT="$(realpath "${0:?}")" || {
-      ui_show_error 'Unable to find myself'
+      ui_error_msg 'Unable to find myself'
       exit 3
     }
-    exec su 0 sh -c "AUTO_ELEVATED=true DEBUG_LOG='${DEBUG_LOG:-0}' FORCE_HW_BUTTONS='${FORCE_HW_BUTTONS:-0}' CI='${CI:-false}' TMPDIR='${TMPDIR:-}' sh -- '${ZIP_INSTALL_SCRIPT:?}' \"\${@}\"" '[su]zip-install.sh' "${@}" || ui_show_error 'failed: exec'
+    exec su 0 sh -c "AUTO_ELEVATED=true DEBUG_LOG='${DEBUG_LOG:-0}' FORCE_HW_BUTTONS='${FORCE_HW_BUTTONS:-0}' CI='${CI:-false}' TMPDIR='${TMPDIR:-}' sh -- '${ZIP_INSTALL_SCRIPT:?}' \"\${@}\"" '[su]zip-install.sh' "${@}" || ui_error_msg 'failed: exec'
     exit "${?}"
   fi
 
-  ui_show_error 'You must execute this as root!!!'
+  ui_error_msg 'You must execute this as root!!!'
   exit 4
 fi
 
@@ -156,11 +173,11 @@ elif test -w '/tmp'; then
   TMPDIR='/tmp'
 elif test -e '/dev'; then
   mkdir -p '/dev/tmp' || {
-    ui_show_error 'Failed to create a temp folder'
+    ui_error_msg 'Failed to create a temp folder'
     exit 9
   }
   chmod 01775 '/dev/tmp' || {
-    ui_show_error "chmod failed on '/dev/tmp'"
+    ui_error_msg "chmod failed on '/dev/tmp'"
     rmdir '/dev/tmp' 2> /dev/null || :
     exit 10
   }
@@ -168,7 +185,7 @@ elif test -e '/dev'; then
 fi
 
 if test -z "${TMPDIR:-}" || test ! -w "${TMPDIR:?}"; then
-  ui_show_error 'Unable to find a temp folder'
+  ui_error_msg 'Unable to find a temp folder'
   exit 11
 fi
 export TMPDIR
@@ -178,11 +195,11 @@ UPD_SCRIPT_NAME="${TMPDIR:?}/updater-script" || exit 12
 ZIPFILE="${1:?}"
 
 unzip -p -qq "${ZIPFILE:?}" 'META-INF/com/google/android/update-binary' 1> "${SCRIPT_NAME:?}" || {
-  ui_show_error "Failed to extract update-binary from => '${ZIPFILE:-}'"
+  ui_error_msg "Failed to extract update-binary from => '${ZIPFILE:-}'"
   exit 13
 }
 test -s "${SCRIPT_NAME:?}" || {
-  ui_show_error "Failed to extract update-binary (2) from => '${ZIPFILE:-}'"
+  ui_error_msg "Failed to extract update-binary (2) from => '${ZIPFILE:-}'"
   exit 14
 }
 
@@ -199,7 +216,7 @@ else
 
   # Legacy versions of chmod don't support +x and --
   chmod 0755 "${SCRIPT_NAME:?}" || {
-    ui_show_error "chmod failed on '${SCRIPT_NAME:?}'"
+    ui_error_msg "chmod failed on '${SCRIPT_NAME:?}'"
     exit 15
   }
   "${SCRIPT_NAME:?}" 3 2 "${ZIPFILE:?}" 'zip-install' "${ZIPINSTALL_VERSION:?}" || STATUS="${?}"
@@ -210,15 +227,15 @@ _clean_at_exit
 trap - 0 2 3 6 15 || : # Already cleaned, so unset traps
 
 if test "${STATUS:-20}" != '0'; then
-  ui_show_error "ZIP installation failed with error ${STATUS:-20}"
+  ui_error_msg "ZIP installation failed with error ${STATUS:-20}"
   exit "${STATUS:-20}"
 fi
 
 # This should theoretically never happen, but it's just to protect against unknown shell bugs
 if test "${END_OF_SCRIPT_REACHED:-0}" != '1'; then
-  ui_show_error "ZIP installation failed with an unknown error"
+  ui_error_msg "ZIP installation failed with an unknown error"
   exit 120
 fi
 
-printf '\033[1;32m%s\033[0m\n' 'The ZIP installation is completed, now restart your device!!!'
+ui_info_msg 'The ZIP installation is completed, now restart your device!!!'
 exit 0
