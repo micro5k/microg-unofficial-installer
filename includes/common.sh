0a5e3d624be72ebd423fdbe332f94de92c1a8837
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2025-02-15 04:01:46 +0100

    Minor changes

--- includes/common.sh
+++ includes/common.sh
@@ -78,7 +78,7 @@ export ftp_proxy="${ftp_proxy-}"
 # shellcheck disable=SC2034
 {
   readonly WGET_CMD='wget'
-  readonly DL_UA='Mozilla/5.0 (Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0'
+  readonly DL_UA='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0'
   readonly DL_ACCEPT_HEADER='Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8'
   readonly DL_ACCEPT_ALL_HEADER='Accept: */*'
   readonly DL_ACCEPT_LANG_HEADER='Accept-Language: en-US,en;q=0.5'
