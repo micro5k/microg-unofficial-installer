db4bc9f881d3bb570671526b615d20a81339267c
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2025-03-07 06:03:52 +0100

    Minor change

--- tools/get-signature.sh
+++ tools/get-signature.sh
@@ -5,7 +5,7 @@
 # shellcheck enable=all
 # shellcheck disable=SC3043 # In POSIX sh, local is undefined
 
-SCRIPT_NAME='Get signature'
-SCRIPT_VERSION='0.0.1'
+export SCRIPT_NAME='Get signature'
+export SCRIPT_VERSION='0.0.1'
 
-"${APKSIGNER_PATH:-apksigner}" verify --min-sdk-version 24 --print-certs -- "${1:?}" | grep -m 1 -F -e 'certificate SHA-256 digest' | cut -d ':' -f '2-' -s | tr -d -- ' ' | tr -- '[:lower:]' '[:upper:]' | sed -e 's/../&:/g;s/:$//'
+"${APKSIGNER_PATH:-apksigner}" verify --min-sdk-version 24  -- "${1:?}" | grep -m 1 -F -e 'certificate SHA-256 digest' | cut -d ':' -f '2-' -s | tr -d -- ' ' | tr -- '[:lower:]' '[:upper:]' | sed -e 's/../&:/g;s/:$//'
