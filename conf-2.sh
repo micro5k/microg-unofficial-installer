51d756a1a543af0a927f0077309b9f3d4c3d815a
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2025-03-20 16:29:53 +0100

    Update links

--- conf-2.sh
+++ conf-2.sh
@@ -6,9 +6,9 @@
 files_to_download()
 {
   cat << 'EOF'
-PlayStore|priv-app|16||Phonesky|com.android.vending|2dc60fc611330609fb11db7193356772aa91d20e|www.apkmirror.com/apk/google-inc/google-play-store/google-play-store-22-6-29-release/google-play-store-22-6-29-16-0-pr-341141187-android-apk-download/|store4.gofile.io/download/web/c8839aa9-c51f-4abf-8b83-f100df903116/PlayStore.apk
-PlayStoreLegacy|priv-app|9|15|Phonesky|com.android.vending|d78b377db43a2bc0570f37b2dd0efa4ec0b95746|www.apkmirror.com/apk/google-inc/google-play-store/google-play-store-5-1-11-3-release/google-play-store-5-1-11-android-apk-download/|store4.gofile.io/download/web/8bfc7a43-3896-476b-9e3b-d223bbd8aaa9/PlayStoreLegacy.apk
-AndroidAuto|priv-app|23||AndroidAuto|com.google.android.projection.gearhead|1ebc63051e5728030eac615210cbdaa25afdc0cb|www.apkmirror.com/apk/google-inc/android-auto/android-auto-1-2-520120-stub-release/android-auto-1-2-520120-stub-android-apk-download/|store4.gofile.io/download/web/a0f2f8c7-e5ba-4410-837b-70a0e8306970/AndroidAuto.apk
-Gmail|app|21|25|PrebuiltGmail|com.google.android.gm|36e13245ddf840698aa8c98543db4f8ab24e2d9c|www.apkmirror.com/apk/google-inc/gmail/gmail-2020-05-31-316831277-release-release/gmail-2020-05-31-316831277-release-3-android-apk-download/|store4.gofile.io/download/web/97f051e5-608c-45ce-bfed-4ec7bd9a4faf/Gmail.apk
+PlayStore|priv-app|16||Phonesky|com.android.vending|2dc60fc611330609fb11db7193356772aa91d20e|www.apkmirror.com/apk/google-inc/google-play-store/google-play-store-22-6-29-release/google-play-store-22-6-29-16-0-pr-341141187-android-apk-download/|store4.gofile.io/download/web/fa9a3df8-bd3e-4f37-9816-31f2806e615e/PlayStore.apk
+PlayStoreLegacy|priv-app|9|15|Phonesky|com.android.vending|d78b377db43a2bc0570f37b2dd0efa4ec0b95746|www.apkmirror.com/apk/google-inc/google-play-store/google-play-store-5-1-11-3-release/google-play-store-5-1-11-android-apk-download/|store2.gofile.io/download/web/d525ebe6-2295-4b0c-8df9-36313479fc8c/PlayStoreLegacy.apk
+AndroidAuto|priv-app|23||AndroidAuto|com.google.android.projection.gearhead|1ebc63051e5728030eac615210cbdaa25afdc0cb|www.apkmirror.com/apk/google-inc/android-auto/android-auto-1-2-520120-stub-release/android-auto-1-2-520120-stub-android-apk-download/|store5.gofile.io/download/web/c73d512b-fef6-4022-b41e-bb7b26b10a35/AndroidAuto.apk
+Gmail|app|21|25|PrebuiltGmail|com.google.android.gm|36e13245ddf840698aa8c98543db4f8ab24e2d9c|www.apkmirror.com/apk/google-inc/gmail/gmail-2020-05-31-316831277-release-release/gmail-2020-05-31-316831277-release-3-android-apk-download/|store10.gofile.io/download/web/80885ebf-cb34-4762-b9d3-94f9a8861e98/Gmail.apk
 EOF
 }
