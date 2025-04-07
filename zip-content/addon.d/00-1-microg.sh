4e4d0dd919d9cb0c0cbf4271dda5cbac13d37f22
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2025-04-07 21:47:12 +0200

    Minor changes

--- zip-content/addon.d/00-1-microg.sh
+++ zip-content/addon.d/00-1-microg.sh
@@ -1,13 +1,11 @@
 #!/sbin/sh
 # ADDOND_VERSION=3
-
 # SPDX-FileCopyrightText: none
 # SPDX-License-Identifier: CC0-1.0
-# SPDX-FileType: SOURCE
 
 # NOTE: The following file come from: https://github.com/LineageOS/android_vendor_lineage/blob/HEAD/prebuilt/common/bin/backuptool.functions
 # shellcheck source=/dev/null
-. '/tmp/backuptool.functions' || { printf 1>&2 'ERROR: Failed to source backuptool.functions\n'; return 9; }
+command . '/tmp/backuptool.functions' || { echo 1>&2 'ERROR: Failed to source backuptool.functions'; return 9; }
 
 list_files()
 {
@@ -18,23 +16,23 @@ EOF
 
 case "${1}" in
   backup)
-    printf '%s\n' 'Backup of microG unofficial installer in progress...'
+    echo 'Backup of microG unofficial installer in progress...'
     list_files | while IFS='|' read -r FILE _; do
       if test -z "${FILE?}"; then continue; fi
-      printf ' %s\n' "${S:?}/${FILE:?}"
+      echo " ${S:?}/${FILE:?}"
       backup_file "${S:?}/${FILE:?}"
     done
-    printf '%s\n' 'Done.'
+    echo 'Done.'
     ;;
   restore)
-    printf '%s\n' 'Restore of microG unofficial installer in progress...'
+    echo 'Restore of microG unofficial installer in progress...'
     list_files | while IFS='|' read -r FILE REPLACEMENT; do
       if test -z "${FILE?}"; then continue; fi
       R=''
       test -n "${REPLACEMENT?}" && R="${S:?}/${REPLACEMENT:?}"
       test -f "${C:?}/${S:?}/${FILE:?}" && restore_file "${S:?}/${FILE:?}" "${R?}"
     done
-    printf '%s\n' 'Done.'
+    echo 'Done.'
     ;;
   pre-backup)
     # Stub
@@ -49,6 +47,6 @@ case "${1}" in
     # Stub
     ;;
   *)
-    printf 1>&2 'WARNING: addon.d unknown phase\n'
+    echo 1>&2 'WARNING: addon.d unknown phase'
     ;;
 esac
