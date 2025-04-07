570a5bd8adbb921cd58f2b3c004a5cf24f8f4bbb
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2025-04-08 00:19:57 +0200

    The addon.d script will now create a log file if enabled with: adb shell "setprop zip.common.DEBUG_LOG 1"

--- zip-content/addon.d/00-1-microg.sh
+++ zip-content/addon.d/00-1-microg.sh
@@ -3,11 +3,28 @@
 # SPDX-FileCopyrightText: none
 # SPDX-License-Identifier: CC0-1.0
 
+_init_debug_log()
+{
+  DEBUG_LOG=0
+  DEBUG_LOG_FILE=''
+
+  if command 1> /dev/null -v 'getprop' && test "$(getprop 'zip.common.DEBUG_LOG' '0' || :)" = 1; then
+    if test -d "${TMPDIR:-/tmp}" && DEBUG_LOG_FILE="${TMPDIR:-/tmp}/debug-a5k.log" && touch "${DEBUG_LOG_FILE:?}"; then DEBUG_LOG=1; fi
+  fi
+}
+
+_display_msg()
+{
+  echo "${1?}"
+  test "${DEBUG_LOG:?}" = 0 || echo "${1?}" 1>> "${DEBUG_LOG_FILE:?}"
+}
+
 # NOTE: The following file come from: https://github.com/LineageOS/android_vendor_lineage/blob/HEAD/prebuilt/common/bin/backuptool.functions
 # shellcheck source=/dev/null
 command . '/tmp/backuptool.functions' || {
-  echo 1>&2 'ERROR: Failed to source backuptool.functions'
-  return 9
+  _init_debug_log
+  _display_msg 1>&2 'ERROR: Failed to source backuptool.functions'
+  return 9 || exit 9
 }
 
 list_files()
@@ -17,25 +34,27 @@ list_files()
 EOF
 }
 
-case "${1}" in
+case "${1-}" in
   backup)
-    echo 'Backup of microG unofficial installer in progress...'
+    _init_debug_log
+    _display_msg 'Backup of microG unofficial installer in progress...'
     list_files | while IFS='|' read -r FILE _; do
       if test -z "${FILE?}"; then continue; fi
-      echo " ${S:?}/${FILE:?}"
+      _display_msg " ${S:?}/${FILE:?}"
       backup_file "${S:?}/${FILE:?}"
     done
-    echo 'Done.'
+    _display_msg 'Done.'
     ;;
   restore)
-    echo 'Restore of microG unofficial installer in progress...'
+    _init_debug_log
+    _display_msg 'Restore of microG unofficial installer in progress...'
     list_files | while IFS='|' read -r FILE REPLACEMENT; do
       if test -z "${FILE?}"; then continue; fi
       R=''
       test -n "${REPLACEMENT?}" && R="${S:?}/${REPLACEMENT:?}"
       test -f "${C:?}/${S:?}/${FILE:?}" && restore_file "${S:?}/${FILE:?}" "${R?}"
     done
-    echo 'Done.'
+    _display_msg 'Done.'
     ;;
   pre-backup)
     # Stub
@@ -50,6 +69,7 @@ case "${1}" in
     # Stub
     ;;
   *)
-    echo 1>&2 'WARNING: addon.d unknown phase'
+    _init_debug_log
+    _display_msg 1>&2 "WARNING: addon.d unknown phase => ${1-}"
     ;;
 esac
