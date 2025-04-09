e442a6f910170b62aae6257c82c57465ee0af1fb
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2025-04-09 14:49:34 +0200

    Minor change

--- zip-content/scripts/install.sh
+++ zip-content/scripts/install.sh
@@ -212,11 +212,13 @@ if test "${DRY_RUN:?}" -eq 0; then
   #fi
 
   # Install survival script
-  if test -e "${SYS_PATH:?}/addon.d"; then
+  if test -d "${SYS_PATH:?}/addon.d"; then
     ui_msg 'Installing survival script...'
     write_file_list "${TMP_PATH}/files" "${TMP_PATH}/files/" "${TMP_PATH}/backup-filelist.lst"
     replace_line_in_file_with_file "${TMP_PATH}/addon.d/00-1-microg.sh" '%PLACEHOLDER-1%' "${TMP_PATH}/backup-filelist.lst"
     copy_file "${TMP_PATH}/addon.d/00-1-microg.sh" "${SYS_PATH}/addon.d"
+  else
+    ui_warning 'addon.d scripts are not supported by your ROM'
   fi
 fi
 
