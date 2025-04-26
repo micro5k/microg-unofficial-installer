bcfbc52c5ce400904da34c401d3d459d924a40f6
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2025-04-26 02:48:16 +0200

    Minor changes

--- tools/list-permissions.sh
+++ tools/list-permissions.sh
@@ -7,6 +7,9 @@
 export SCRIPT_NAME='List permissions used by apps'
 export SCRIPT_VERSION='0.0.1'
 
+# shellcheck disable=SC3040 # Ignore: In POSIX sh, set option pipefail is undefined
+case "$(set 2> /dev/null -o || set || :)" in *'pipefail'*) set -o pipefail || echo 1>&2 'Failed: pipefail' ;; *) ;; esac
+
 main()
 {
   test -n "${AAPT2_PATH-}" || AAPT2_PATH="$(command -v 'aapt2')" || AAPT2_PATH='' || :
@@ -14,7 +17,7 @@ main()
   if test -n "${AAPT2_PATH?}"; then
     "${AAPT2_PATH:?}" dump permissions "${@}" | grep -F -e 'uses-permission: ' | cut -d ':' -f '2-' -s | cut -b '2-' || return "${?}"
   else
-    return 1
+    return 255
   fi
 }
 
