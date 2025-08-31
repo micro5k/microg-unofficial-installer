eef8d1e818fcc05fdd438912a16a41e2150bc2c3
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2025-08-31 22:19:27 +0200

    Minor change

--- tools/dl-perm-list.sh
+++ tools/dl-perm-list.sh
@@ -19,7 +19,6 @@ set -u
 # shellcheck disable=SC3040,SC3041,SC2015
 {
   # Unsupported set options may cause the shell to exit (even without set -e), so first try them in a subshell to avoid this issue
-  (set -o posix 2> /dev/null) && set -o posix || true
   (set +H 2> /dev/null) && set +H || true
   (set -o pipefail 2> /dev/null) && set -o pipefail || true
 }
