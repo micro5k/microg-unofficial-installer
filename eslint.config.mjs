e30e89ea245771adb62782b0b9a599701ec3c49b
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2026-03-06 18:17:46 +0100

    chore: minor changes
    Signed-off-by: ale5000 <15793015+ale5000-git@users.noreply.github.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,13 @@
-import { defineConfig } from "eslint/config";
+// SPDX-FileCopyrightText: (c) 2026 ale5000
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import {
+  defineConfig
+}
+from 'eslint/config';
 
 export default defineConfig([{
-    languageOptions: {
-        ecmaVersion: 2022
-    }
+  languageOptions: {
+    ecmaVersion: 2022
+  }
 }]);
