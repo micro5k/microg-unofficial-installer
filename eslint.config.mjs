b525e837f01756a1e4fcd71bea79025ccb733a6c
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2026-03-19 23:47:04 +0100

    chore: minor changes
    Signed-off-by: ale5000 <15793015+ale5000-git@users.noreply.github.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,23 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 /* jshint esversion: 6 */
-import {
-  defineConfig
-}
-from 'eslint/config';
+import js from '@eslint/js';
+import json from '@eslint/json';
+import markdown from '@eslint/markdown';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
-export default defineConfig([{
-  languageOptions: {
-    ecmaVersion: 2022
-  }
-}]);
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node, ecmaVersion: 2022, sourceType: 'module' },
+    rules: {
+      'no-unused-vars': 'warn',
+      semi: ['warn', 'always'],
+    },
+  },
+  { files: ['**/*.json'], plugins: { json }, language: 'json/json', extends: ['json/recommended'] },
+  { files: ['**/*.md'], plugins: { markdown }, language: 'markdown/gfm', extends: ['markdown/recommended'] },
+]);
