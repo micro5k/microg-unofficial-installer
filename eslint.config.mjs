7813da409814c2712d8021b7bbb7608b7398ad74
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2026-03-20 00:52:40 +0100

    chore: minor changes
    Signed-off-by: ale5000 <15793015+ale5000-git@users.noreply.github.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,12 +13,26 @@ export default defineConfig([
     files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
     extends: ['js/recommended'],
-    languageOptions: { globals: globals.node, ecmaVersion: 2022, sourceType: 'module' },
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
     rules: {
       'no-unused-vars': 'warn',
       semi: ['error', 'always'],
     },
   },
-  { files: ['**/*.json'], plugins: { json }, language: 'json/json', extends: ['json/recommended'] },
-  { files: ['**/*.md'], plugins: { markdown }, language: 'markdown/gfm', extends: ['markdown/recommended'] },
+  {
+    files: ['**/*.json'],
+    plugins: { json },
+    language: 'json/json',
+    extends: ['json/recommended'],
+  },
+  {
+    files: ['**/*.md'],
+    plugins: { markdown },
+    language: 'markdown/gfm',
+    extends: ['markdown/recommended'],
+  },
 ]);
