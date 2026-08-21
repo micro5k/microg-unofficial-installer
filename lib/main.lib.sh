756e34bdbcfeeee3bf1eb8e4ab06ff41fc6c49a5
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2026-08-21 12:07:50 +0200

    chore: minor changes
    Signed-off-by: ale5000 <15793015+ale5000-git@users.noreply.github.com>

--- lib/main.lib.sh
+++ lib/main.lib.sh
@@ -176,17 +176,17 @@ get_shell_exe()
     printf '%s\n' "${_gse_shell_exe}"
     return 0
   elif _gse_tmp_var="$(ps 2> /dev/null -p "${$}" -o 'comm=')" && test -n "${_gse_tmp_var}" && _gse_tmp_var="$(command 2> /dev/null -v "${_gse_tmp_var}")"; then
-    # On Linux / macOS
-    # shellcheck disable=SC2230 # Ignore: 'which' is non-standard
-    case "${_gse_tmp_var}" in *'/'* | *"\\"*) ;; *) _gse_tmp_var="$(which 2> /dev/null "${_gse_tmp_var}")" || return 3 ;; esac # We may not get the full path with "command -v" on some old versions of Oils
+    # On Linux / macOS / BSD
+    case "${_gse_tmp_var}" in *'/'* | *"\\"*) ;; *) _gse_tmp_var="$(command 2> /dev/null -v "${_gse_tmp_var}")" || return 2 ;; esac
   elif _gse_tmp_var="${BASH:-${SHELL-}}" && test -n "${_gse_tmp_var}"; then
-    if test "${_gse_tmp_var}" = '/bin/sh' && test "$(uname 2> /dev/null || :)" = 'Windows_NT'; then _gse_tmp_var="$(command 2> /dev/null -v 'busybox')" || return 2; fi
-    if test ! -x "${_gse_tmp_var}" && test -x "${_gse_tmp_var}.exe"; then _gse_tmp_var="${_gse_tmp_var}.exe"; fi # Special fix for broken versions of Bash under Windows
+    if test "${_gse_tmp_var}" = '/bin/sh' && test "$(uname 2> /dev/null || :)" = 'Windows_NT'; then _gse_tmp_var="$(command 2> /dev/null -v 'busybox')" || return 3; fi
+    # shellcheck disable=SC1003 # IGNORE: Want to escape a single quote? echo 'This is how it'\''s done'
+    case "${_gse_tmp_var}" in *'/'*) ;; *"\\"*) _gse_tmp_var="$(printf '%s' "${_gse_tmp_var}" | tr '\\' '/')" || return 4 ;; *) ;; esac
   else
     return 1
   fi
 
-  _gse_shell_exe="$(readlink 2> /dev/null -f "${_gse_tmp_var}" || realpath 2> /dev/null "${_gse_tmp_var}")" || _gse_shell_exe="${_gse_tmp_var}"
+  _gse_shell_exe="$(realpath 2> /dev/null "${_gse_tmp_var}" || readlink 2> /dev/null -f "${_gse_tmp_var}")" || _gse_shell_exe="${_gse_tmp_var}"
   printf '%s\n' "${_gse_shell_exe}"
   return 0
 }
