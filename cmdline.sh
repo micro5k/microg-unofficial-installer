0e33ae94dd48c796ddc9d41238bb410f4cf981b3
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2026-08-22 13:55:23 +0200

    chore: minor changes
    Signed-off-by: ale5000 <15793015+ale5000-git@users.noreply.github.com>

--- cmdline.sh
+++ cmdline.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 # SPDX-FileCopyrightText: 2016 ale5000
 # SPDX-License-Identifier: GPL-3.0-or-later WITH LicenseRef-Archive-packaging-exception
+
 # shellcheck enable=all
+# shellcheck disable=SC2310 # IGNORE: check-set-e-suppressed
 
 # Workaround for shells without support for local (example: ksh pbosh obosh)
 command 1> /dev/null 2>&1 -v 'local' || {
