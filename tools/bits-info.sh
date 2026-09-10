3703a7676f98c61f6136af6d8f8fb37f823c4a4f
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2026-09-10 12:11:50 +0200

    chore: minor changes
    Signed-off-by: ale5000 <15793015+ale5000-git@users.noreply.github.com>

--- tools/bits-info.sh
+++ tools/bits-info.sh
@@ -31,10 +31,10 @@ SCRIPT_YEAR='2024'
 set -u 2> /dev/null || :
 # shellcheck disable=SC3040 # IGNORE: In POSIX sh, set option pipefail is undefined
 case "$(set -o 2> /dev/null || set || :)" in *'pipefail'*) set -o pipefail || echo 1>&2 'ERROR: pipefail failed' ;; *) ;; esac
-# IMPORTANT: Double-clicking a script file on Windows opens Bash as an interactive shell and enables 'history', 'histexpand' and 'monitor' contrary to any logic
+# IMPORTANT: Double-clicking a script file on Windows opens Bash as an interactive shell and enables 'histexpand', 'history' and 'monitor' contrary to any logic
 # shellcheck disable=SC3040 # IGNORE: In POSIX sh, set option 'foo' is undefined
 if test -f '/usr/bin/cygpath'; then
-  set +o histexpand || :
+  (set +o histexpand 2> /dev/null) && set +o histexpand || :
   set +o history || :
   set +o monitor || :
 fi
